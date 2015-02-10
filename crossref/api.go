30cc5a71dd82a1c6aba1f64101924d7782a7daee
Martin Czygan <martin.czygan@gmail.com>
2015-02-10 16:31:47 +0100

    add crossref member lookup function
    Uses an in-memory cache for responses.

--- crossref/api.go
+++ crossref/api.go
@@ -1,262 +1,80 @@
-// Package crossref implements crossref related structs and transformations.
-//
-// API endpoint/documentation: http://api.crossref.org
 package crossref
 
 import (
-	"bytes"
-	"encoding/base64"
-	"errors"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
-	"strconv"
-	"strings"
-	"time"
-
-	"github.com/miku/span"
-	"github.com/miku/span/finc"
-	"github.com/miku/span/holdings"
+	"net/http"
+	"sync"
 )
 
-// Author is given by family and given name
-type Author struct {
-	Family string `json:"family"`
-	Given  string `json:"given"`
+// Message covers a generic API response.
+type Message struct {
+	Status  string          `json:"status"`
+	Version string          `json:"message-version"`
+	Message json.RawMessage `json:"message"`
+	Type    string          `json:"message-type"`
 }
 
-// String pretty print the author
-func (author *Author) String() string {
-	if author.Given != "" {
-		return fmt.Sprintf("%s, %s", author.Family, author.Given)
-	} else {
-		return author.Family
-	}
+// Member covers a member type message.
+type Member struct {
+	ID          int      `json:"id"`
+	PrimaryName string   `json:"primary-name"`
+	Names       []string `json:"names"`
+	Location    string   `json:"location"`
+	Prefixes    []string `json:"prefixes"`
+	Tokens      []string `json:"tokens"`
 }
 
-// DatePart consists of up to three int, representing year, month, day
-type DatePart []int
-
-// DateField contains two representations of one value
-type DateField struct {
-	Timestamp int64      `json:"timestamp"`
-	DateParts []DatePart `json:"date-parts"`
+// Cache for members, thread-safe.
+type Cache struct {
+	lock    *sync.RWMutex
+	Entries map[int]Member
 }
 
-// Document is a example API response
-type Document struct {
-	Prefix         string    `json:"prefix"`
-	Type           string    `json:"type"`
-	Volume         string    `json:"volume"`
-	Deposited      DateField `json:"deposited"`
-	Source         string    `json:"source"`
-	Authors        []Author  `json:"author"`
-	Score          float64   `json:"score"`
-	Page           string    `json:"page"`
-	Subject        []string  `json:"subject"`
-	Title          []string  `json:"title"`
-	Publisher      string    `json:"publisher"`
-	ISSN           []string  `json:"ISSN"`
-	Indexed        DateField `json:"indexed"`
-	Issued         DateField `json:"issued"`
-	Subtitle       []string  `json:"subtitle"`
-	URL            string    `json:"URL"`
-	Issue          string    `json:"issue"`
-	ContainerTitle []string  `json:"container-title"`
-	ReferenceCount int       `json:"reference-count"`
-	DOI            string    `json:"DOI"`
+// NewMemberCache creates a new in memory members cache.
+func NewCache() Cache {
+	return Cache{Entries: make(map[int]Member), lock: new(sync.RWMutex)}
 }
 
-// Year returns the *first* year found inside a DateField
-func (d *DateField) Year() int {
-	parts := d.DateParts
-	if len(parts) >= 1 {
-		if len(parts[0]) > 0 {
-			return parts[0][0]
-		}
-	}
-	return 0
-}
+var cache Cache = NewCache()
 
-// Date returns a time.Date in a best effort way
-func (d *DateField) Date() (t time.Time) {
-	if len(d.DateParts) == 0 {
-		t, _ = time.Parse("2006-01-02", "0000-00-00")
-	}
-	p := d.DateParts[0]
-	switch len(p) {
-	case 1:
-		t, _ = time.Parse("2006-01-02", fmt.Sprintf("%04d-01-01", p[0]))
-	case 2:
-		t, _ = time.Parse("2006-01-02", fmt.Sprintf("%04d-%02d-01", p[0], p[1]))
-	case 3:
-		t, _ = time.Parse("2006-01-02", fmt.Sprintf("%04d-%02d-%02d", p[0], p[1], p[2]))
-	default:
-		t, _ = time.Parse("2006-01-02", "1970-01-01")
-	}
-	return t
-}
-
-// CombinedTitle returns a longish title
-func (d *Document) CombinedTitle() string {
-	if len(d.Title) > 0 {
-		if len(d.Subtitle) > 0 {
-			return fmt.Sprintf("%s : %s", d.Title[0], d.Subtitle[0])
-		} else {
-			return d.Title[0]
-		}
-	} else {
-		if len(d.Subtitle) > 0 {
-			return d.Subtitle[0]
-		} else {
-			return ""
-		}
-	}
-}
-
-// FullTitle returns everything title
-func (d *Document) FullTitle() string {
-	return strings.Join(append(d.Title, d.Subtitle...), " ")
-}
-
-// ShortTitle returns the first main title only
-func (d *Document) ShortTitle() string {
-	if len(d.Title) > 0 {
-		return d.Title[0]
-	} else {
-		return ""
-	}
-}
-
-// CoveredBy returns nil, if a given entitlement covers the current document
-func (d *Document) CoveredBy(e holdings.Entitlement) error {
-	if e.FromYear != 0 && e.FromYear > d.Issued.Year() {
-		return fmt.Errorf("from-year %d > %d", e.FromYear, d.Issued.Year())
-	}
-	if e.FromYear == d.Issued.Year() {
-		volume, err := strconv.Atoi(d.Volume)
+// LookupMember retrieves member information via crossref API.
+// Subsequent requests are served by an in-memory cache.
+// Example: http://api.crossref.org/members/56
+func LookupMember(id int) (Member, error) {
+	member, ok := cache.Entries[id]
+	if !ok {
+		link := fmt.Sprintf("http://api.crossref.org/members/%d", id)
+		log.Printf("Fetching: %s", link)
+		resp, err := http.Get(link)
 		if err != nil {
-			return err
+			return member, err
 		}
-		if e.FromVolume != 0 && e.FromVolume > volume {
-			return fmt.Errorf("from-volume %d > %d", e.FromVolume, volume)
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return member, err
 		}
-		if e.FromVolume == volume {
-			issue, err := strconv.Atoi(d.Issue)
+		var message Message
+		err = json.Unmarshal(body, &message)
+		if err != nil {
+			return member, err
+		}
+		if message.Type == "member" {
+			err = json.Unmarshal(message.Message, &member)
 			if err != nil {
-				return err
+				return member, err
 			}
-			if e.FromIssue != 0 && e.FromIssue > issue {
-				return fmt.Errorf("from-issue %d > %d", e.FromIssue, issue)
+			cache.lock.Lock()
+			if _, ok := cache.Entries[id]; !ok {
+				cache.Entries[id] = member
 			}
+			cache.lock.Unlock()
+		} else {
+			return member, fmt.Errorf("unsupported message type: %s", message.Type)
 		}
 	}
-	if e.ToYear != 0 && e.ToYear < d.Issued.Year() {
-		return fmt.Errorf("to-year %d < %d", e.ToYear, d.Issued.Year())
-	}
-	if e.ToYear == d.Issued.Year() {
-		volume, err := strconv.Atoi(d.Volume)
-		if err != nil {
-			return err
-		}
-		if e.ToVolume != 0 && e.ToVolume < volume {
-			return fmt.Errorf("to-volume %d < %d", e.ToVolume, volume)
-		}
-		if e.ToVolume == volume {
-			issue, err := strconv.Atoi(d.Issue)
-			if err != nil {
-				return err
-			}
-			if e.ToIssue != 0 && e.ToIssue < issue {
-				return fmt.Errorf("to-issue %d < %d", e.ToIssue, issue)
-			}
-		}
-	}
-	boundary, err := e.Boundary()
-	if err != nil {
-		return err
-	}
-	if d.Issued.Date().After(boundary) {
-		return fmt.Errorf("moving-wall %s %s", boundary, d.Issued.Date())
-	}
-	return nil
-
-}
-
-// ToSchema converts a single crossref document into a basic finc.Schema
-func (d Document) ToSchema() (output finc.Schema, err error) {
-	if d.URL == "" {
-		return output, errors.New("input document has no URL")
-	}
-
-	encoded := base64.StdEncoding.EncodeToString([]byte(d.URL))
-	output.ID = fmt.Sprintf("ai049%s", encoded)
-	output.ISSN = d.ISSN
-	output.Publisher = d.Publisher
-	output.SourceID = "49"
-	output.RecordType = "ai"
-	output.Title = d.CombinedTitle()
-	output.TitleFull = d.FullTitle()
-	output.TitleShort = d.ShortTitle()
-	output.Topics = d.Subject
-	output.URL = d.URL
-
-	if len(d.ContainerTitle) > 0 {
-		output.HierarchyParentTitle = d.ContainerTitle[0]
-	}
-
-	if d.Type == "journal-article" {
-		output.Format = "ElectronicArticle"
-	}
-
-	for _, author := range d.Authors {
-		output.SecondaryAuthors = append(output.SecondaryAuthors, author.String())
-	}
-
-	if d.Issued.Year() > 0 {
-		output.PublishDateSort = d.Issued.Year()
-	}
-
-	allfields := [][]string{output.SecondaryAuthors, d.Subject, d.ISSN, d.Title,
-		d.Subtitle, d.ContainerTitle, []string{d.Publisher, d.URL}}
-
-	var buf bytes.Buffer
-	for _, f := range allfields {
-		_, err := buf.WriteString(fmt.Sprintf("%s ", strings.Join(f, " ")))
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	output.Allfields = buf.String()
-	return output, nil
-}
-
-// Institutions returns a slice of ISILs for which this document finds
-// valid entitlements in a IsilIssnHolding map
-func (d *Document) Institutions(iih holdings.IsilIssnHolding) []string {
-	isils := span.NewStringSet()
-	for _, isil := range iih.Isils() {
-		covered := false
-		for _, issn := range d.ISSN {
-			h, ok := iih[isil][issn]
-			if ok {
-				for _, entitlement := range h.Entitlements {
-					err := d.CoveredBy(entitlement)
-					if err == nil {
-						covered = true
-					}
-					if covered {
-						break
-					}
-				}
-			}
-			if covered {
-				break
-			}
-		}
-		if covered {
-			isils.Add(isil)
-		}
-	}
-	return isils.Values()
+	return member, nil
 }
---
+++ crossref/document.go
@@ -0,0 +1,280 @@
+// Package crossref implements crossref related structs and transformations.
+//
+// API endpoint/documentation: http://api.crossref.org
+package crossref
+
+import (
+	"bytes"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/miku/span"
+	"github.com/miku/span/finc"
+	"github.com/miku/span/holdings"
+)
+
+// Author is given by family and given name
+type Author struct {
+	Family string `json:"family"`
+	Given  string `json:"given"`
+}
+
+// String pretty print the author
+func (author *Author) String() string {
+	if author.Given != "" {
+		return fmt.Sprintf("%s, %s", author.Family, author.Given)
+	} else {
+		return author.Family
+	}
+}
+
+// DatePart consists of up to three int, representing year, month, day
+type DatePart []int
+
+// DateField contains two representations of one value
+type DateField struct {
+	Timestamp int64      `json:"timestamp"`
+	DateParts []DatePart `json:"date-parts"`
+}
+
+// Document is a example API response
+type Document struct {
+	Prefix         string    `json:"prefix"`
+	Type           string    `json:"type"`
+	Volume         string    `json:"volume"`
+	Deposited      DateField `json:"deposited"`
+	Source         string    `json:"source"`
+	Authors        []Author  `json:"author"`
+	Score          float64   `json:"score"`
+	Page           string    `json:"page"`
+	Subject        []string  `json:"subject"`
+	Title          []string  `json:"title"`
+	Publisher      string    `json:"publisher"`
+	ISSN           []string  `json:"ISSN"`
+	Indexed        DateField `json:"indexed"`
+	Issued         DateField `json:"issued"`
+	Subtitle       []string  `json:"subtitle"`
+	URL            string    `json:"URL"`
+	Issue          string    `json:"issue"`
+	ContainerTitle []string  `json:"container-title"`
+	ReferenceCount int       `json:"reference-count"`
+	DOI            string    `json:"DOI"`
+	Member         string    `json:"member"`
+}
+
+// Year returns the first year found inside a DateField.
+func (d *DateField) Year() int {
+	parts := d.DateParts
+	if len(parts) >= 1 {
+		if len(parts[0]) > 0 {
+			return parts[0][0]
+		}
+	}
+	return 0
+}
+
+// Date returns a time.Date in a best effort way.
+func (d *DateField) Date() (t time.Time) {
+	if len(d.DateParts) == 0 {
+		t, _ = time.Parse("2006-01-02", "0000-00-00")
+	}
+	p := d.DateParts[0]
+	switch len(p) {
+	case 1:
+		t, _ = time.Parse("2006-01-02", fmt.Sprintf("%04d-01-01", p[0]))
+	case 2:
+		t, _ = time.Parse("2006-01-02", fmt.Sprintf("%04d-%02d-01", p[0], p[1]))
+	case 3:
+		t, _ = time.Parse("2006-01-02", fmt.Sprintf("%04d-%02d-%02d", p[0], p[1], p[2]))
+	default:
+		t, _ = time.Parse("2006-01-02", "1970-01-01")
+	}
+	return t
+}
+
+// CombinedTitle returns a longish title.
+func (d *Document) CombinedTitle() string {
+	if len(d.Title) > 0 {
+		if len(d.Subtitle) > 0 {
+			return fmt.Sprintf("%s : %s", d.Title[0], d.Subtitle[0])
+		} else {
+			return d.Title[0]
+		}
+	} else {
+		if len(d.Subtitle) > 0 {
+			return d.Subtitle[0]
+		} else {
+			return ""
+		}
+	}
+}
+
+// FullTitle returns everything title.
+func (d *Document) FullTitle() string {
+	return strings.Join(append(d.Title, d.Subtitle...), " ")
+}
+
+// ShortTitle returns the first main title only.
+func (d *Document) ShortTitle() string {
+	if len(d.Title) > 0 {
+		return d.Title[0]
+	} else {
+		return ""
+	}
+}
+
+// CoveredBy returns nil, if a given entitlement covers the current document.
+// Otherwise the error con
+func (d *Document) CoveredBy(e holdings.Entitlement) error {
+	if e.FromYear != 0 && e.FromYear > d.Issued.Year() {
+		return fmt.Errorf("from-year %d > %d", e.FromYear, d.Issued.Year())
+	}
+	if e.FromYear == d.Issued.Year() {
+		volume, err := strconv.Atoi(d.Volume)
+		if err != nil {
+			return err
+		}
+		if e.FromVolume != 0 && e.FromVolume > volume {
+			return fmt.Errorf("from-volume %d > %d", e.FromVolume, volume)
+		}
+		if e.FromVolume == volume {
+			issue, err := strconv.Atoi(d.Issue)
+			if err != nil {
+				return err
+			}
+			if e.FromIssue != 0 && e.FromIssue > issue {
+				return fmt.Errorf("from-issue %d > %d", e.FromIssue, issue)
+			}
+		}
+	}
+	if e.ToYear != 0 && e.ToYear < d.Issued.Year() {
+		return fmt.Errorf("to-year %d < %d", e.ToYear, d.Issued.Year())
+	}
+	if e.ToYear == d.Issued.Year() {
+		volume, err := strconv.Atoi(d.Volume)
+		if err != nil {
+			return err
+		}
+		if e.ToVolume != 0 && e.ToVolume < volume {
+			return fmt.Errorf("to-volume %d < %d", e.ToVolume, volume)
+		}
+		if e.ToVolume == volume {
+			issue, err := strconv.Atoi(d.Issue)
+			if err != nil {
+				return err
+			}
+			if e.ToIssue != 0 && e.ToIssue < issue {
+				return fmt.Errorf("to-issue %d < %d", e.ToIssue, issue)
+			}
+		}
+	}
+	boundary, err := e.Boundary()
+	if err != nil {
+		return err
+	}
+	if d.Issued.Date().After(boundary) {
+		return fmt.Errorf("moving-wall %s %s", boundary, d.Issued.Date())
+	}
+	return nil
+
+}
+
+// ToSchema converts a single crossref document into a basic finc schema.
+func (d Document) ToSchema() (output finc.Schema, err error) {
+	if d.URL == "" {
+		return output, errors.New("input document has no URL")
+	}
+
+	encoded := base64.StdEncoding.EncodeToString([]byte(d.URL))
+	output.ID = fmt.Sprintf("ai049%s", encoded)
+	output.ISSN = d.ISSN
+	output.Publisher = d.Publisher
+	output.SourceID = "49"
+	output.RecordType = "ai"
+	output.Title = d.CombinedTitle()
+	output.TitleFull = d.FullTitle()
+	output.TitleShort = d.ShortTitle()
+	output.Topics = d.Subject
+	output.URL = d.URL
+
+	if len(d.ContainerTitle) > 0 {
+		output.HierarchyParentTitle = d.ContainerTitle[0]
+	}
+
+	if d.Type == "journal-article" {
+		output.Format = "ElectronicArticle"
+	}
+
+	for _, author := range d.Authors {
+		output.SecondaryAuthors = append(output.SecondaryAuthors, author.String())
+	}
+
+	if d.Issued.Year() > 0 {
+		output.PublishDateSort = d.Issued.Year()
+	}
+
+	allfields := [][]string{output.SecondaryAuthors, d.Subject, d.ISSN, d.Title,
+		d.Subtitle, d.ContainerTitle, []string{d.Publisher, d.URL}}
+
+	var buf bytes.Buffer
+	for _, f := range allfields {
+		_, err := buf.WriteString(fmt.Sprintf("%s ", strings.Join(f, " ")))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	output.Allfields = buf.String()
+
+	fields := strings.Split(d.Member, "/")
+	if len(fields) > 0 {
+		id, err := strconv.Atoi(fields[len(fields)-1])
+		if err != nil {
+			log.Printf("invalid member: %s", d.Member)
+		} else {
+			member, err := LookupMember(id)
+			if err != nil {
+				log.Println(err)
+			} else {
+				output.MegaCollection = fmt.Sprintf("%s (CrossRef)", member.PrimaryName)
+			}
+		}
+	}
+
+	return output, nil
+}
+
+// Institutions returns a slice of ISILs for which this document finds
+// valid entitlements in a IsilIssnHolding map.
+func (d *Document) Institutions(iih holdings.IsilIssnHolding) []string {
+	isils := span.NewStringSet()
+	for _, isil := range iih.Isils() {
+		covered := false
+		for _, issn := range d.ISSN {
+			h, ok := iih[isil][issn]
+			if ok {
+				for _, entitlement := range h.Entitlements {
+					err := d.CoveredBy(entitlement)
+					if err == nil {
+						covered = true
+					}
+					if covered {
+						break
+					}
+				}
+			}
+			if covered {
+				break
+			}
+		}
+		if covered {
+			isils.Add(isil)
+		}
+	}
+	return isils.Values()
+}
