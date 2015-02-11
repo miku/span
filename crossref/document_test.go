57c78f6af99df2f955562785544ba9c90acfde4c
Martin Czygan <martin.czygan@gmail.com>
2015-02-11 14:39:34 +0100

    update document test

--- crossref/document_test.go
+++ crossref/document_test.go
@@ -1,6 +1,7 @@
 package crossref
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/miku/span/holdings"
@@ -12,9 +13,20 @@ func TestCoveredBy(t *testing.T) {
 		e   holdings.Entitlement
 		err error
 	}{
-		{doc: Document{}, e: holdings.Entitlement{}},
+		{doc: Document{Issued: DateField{DateParts: []DatePart{DatePart{2000, 1, 1}}}}, e: holdings.Entitlement{}},
+		{doc: Document{Issued: DateField{DateParts: []DatePart{DatePart{2000, 1, 1}}}}, e: holdings.Entitlement{FromDelay: "-100Y"}, err: fmt.Errorf("moving-wall violation")},
 	}
 
 	for _, tt := range tests {
+		err := tt.doc.CoveredBy(tt.e)
+		if err != tt.err {
+			if err != nil && tt.err != nil {
+				if err.Error() != tt.err.Error() {
+					t.Errorf("got: %v, want: %s", err, tt.err)
+				}
+			} else {
+				t.Errorf("got: %v, want: %s", err, tt.err)
+			}
+		}
 	}
 }
