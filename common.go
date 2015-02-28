63255a84bbd806c0be19d34052d6fb56099b2971
Martin Czygan <martin.czygan@gmail.com>
2015-02-28 14:42:28 +0100

    get rid of converter interfaces

--- common.go
+++ common.go
@@ -3,22 +3,10 @@ package span
 import (
 	"fmt"
 	"strings"
-
-	"github.com/miku/span/finc"
 )
 
 // Version of span
 const Version = "0.1.7"
-
-// FincConverter for finc.Schema output format.
-type FincConverter interface {
-	ToSchema() (finc.Schema, error)
-}
-
-// FincSolrConverter for finc.SolrSchema output format.
-type FincSolrConverter interface {
-	ToSolrSchema() (finc.SolrSchema, error)
-}
 
 // ParseHoldingSpec parses a holdings flag value into a map.
 func ParseHoldingSpec(s string) (map[string]string, error) {
