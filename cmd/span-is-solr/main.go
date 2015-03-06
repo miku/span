556902fd380047c5ba22d8592bf07f5af107bb02
Martin Czygan <martin.czygan@gmail.com>
2015-03-06 19:38:42 +0100

    prepare ISIL lookup

--- cmd/span-is-solr/main.go
+++ cmd/span-is-solr/main.go
@@ -10,15 +10,43 @@ import (
 	"log"
 	"os"
 
+	"github.com/miku/span"
 	"github.com/miku/span/finc"
+	"github.com/miku/span/holdings"
 )
 
+// Options for worker.
+type Options struct {
+	Holdings holdings.IsilIssnHolding
+}
+
 func main() {
+
+	hspec := flag.String("hspec", "", "ISIL PATH pairs")
 
 	flag.Parse()
 
 	if flag.NArg() < 1 {
 		log.Fatal("input file required")
+	}
+
+	options := Options{
+		Holdings: make(holdings.IsilIssnHolding),
+	}
+
+	if *hspec != "" {
+		pathmap, err := span.ParseHoldingSpec(*hspec)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for isil, path := range pathmap {
+			file, err := os.Open(path)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer file.Close()
+			options.Holdings[isil] = holdings.HoldingsMap(bufio.NewReader(file))
+		}
 	}
 
 	for _, filename := range flag.Args() {
@@ -41,6 +69,7 @@ func main() {
 				log.Fatal(err)
 			}
 			ss, err := is.ToSolrSchema()
+			ss.Institutions = is.Institutions(options.Holdings)
 			if err != nil {
 				log.Fatal(err)
 			}
