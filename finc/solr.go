40f1694e1a5f6c900fa1b5441f153b482026d6f7
Martin Czygan <martin.czygan@gmail.com>
2015-03-13 21:31:16 +0100

    omit empty values, so solr can apply its defaults

--- finc/solr.go
+++ finc/solr.go
@@ -2,29 +2,29 @@ package finc
 
 // SolrSchema represents a finc schema, evolving as needed.
 type SolrSchema struct {
-	AccessFacet          string   `json:"access_facet"`
-	Allfields            string   `json:"allfields"`
-	Author               string   `json:"author"`
-	FincClassFacet       []string `json:"finc_class_facet"`
-	Formats              []string `json:"format"`
-	Fullrecord           string   `json:"fullrecord"`
-	HierarchyParentTitle string   `json:"hierarchy_parent_title"`
-	ID                   string   `json:"id"`
-	Institutions         []string `json:"institution"`
-	ISSN                 []string `json:"issn"`
-	Languages            []string `json:"language"`
-	MegaCollection       []string `json:"mega_collection"`
-	PublishDateSort      int      `json:"publishDateSort"`
-	Publishers           []string `json:"publisher"`
-	RecordType           string   `json:"recordtype"`
-	SecondaryAuthors     []string `json:"author2"`
-	SourceID             string   `json:"source_id"`
-	Title                string   `json:"title"`
+	AccessFacet          string   `json:"access_facet,omitempty"`
+	Allfields            string   `json:"allfields,omitempty"`
+	Author               string   `json:"author,omitempty"`
+	FincClassFacet       []string `json:"finc_class_facet,omitempty"`
+	Formats              []string `json:"format,omitempty"`
+	Fullrecord           string   `json:"fullrecord,omitempty"`
+	HierarchyParentTitle string   `json:"hierarchy_parent_title,omitempty"`
+	ID                   string   `json:"id,omitempty"`
+	Institutions         []string `json:"institution,omitempty"`
+	ISSN                 []string `json:"issn,omitempty"`
+	Languages            []string `json:"language,omitempty"`
+	MegaCollection       []string `json:"mega_collection,omitempty"`
+	PublishDateSort      int      `json:"publishDateSort,omitempty"`
+	Publishers           []string `json:"publisher,omitempty"`
+	RecordType           string   `json:"recordtype,omitempty"`
+	SecondaryAuthors     []string `json:"author2,omitempty"`
+	SourceID             string   `json:"source_id,omitempty"`
+	Title                string   `json:"title,omitempty"`
 	TitleFull            string   `json:"title_full"`
-	TitleShort           string   `json:"title_short"`
-	Topics               []string `json:"topic"`
-	URL                  string   `json:"url"`
+	TitleShort           string   `json:"title_short,omitempty"`
+	Topics               []string `json:"topic,omitempty"`
+	URL                  string   `json:"url,omitempty"`
 
 	// TODO(miku): sanitize this, quickly!
-	FormatDe15 string `json:"format_de15"`
+	FormatDe15 string `json:"format_de15,omitempty"`
 }
