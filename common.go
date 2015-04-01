501003fcb42275bb36b70133f288270728aa78ef
Martin Czygan <martin.czygan@gmail.com>
2015-04-01 14:44:34 +0200

    v0.1.27

--- common.go
+++ common.go
@@ -12,7 +12,7 @@ import (
 )
 
 // AppVersion of span package. Commandline tools will show this on -v.
-const AppVersion = "0.1.26"
+const AppVersion = "0.1.27"
 
 // Batcher groups strings together for batched processing.
 // It is more effective to send one batch over a channel than many strings.
