bf8dce33efdfcefabb74ef2505e51c0e4fbaa420
Martin Czygan <martin.czygan@gmail.com>
2015-05-22 14:25:58 +0200

    factor out some common things

--- cmd/span-holdings/main.go
+++ cmd/span-holdings/main.go
@@ -21,12 +21,26 @@ type TagPath struct {
 	Path string
 }
 
-func parseTagPath(s string) (TagPath, error) {
+// parseTagPathString turns TAG:/path/to into single strings and returns them.
+func parseTagPathString(s string) (string, string, error) {
 	p := strings.Split(s, ":")
 	if len(p) != 2 {
-		return TagPath{}, errors.New("invalid TagPath, use ISIL:/path/to/file")
+		return "", "", errors.New("invalid tagpath, use ISIL:/path/to/file")
 	}
-	return TagPath{Tag: p[0], Path: p[1]}, nil
+	return p[0], p[1], nil
+}
+
+func parseTagPath(s string) (string, *os.File, error) {
+	var file *os.File
+	isil, path, err := parseTagPathString(s)
+	if err != nil {
+		return isil, file, err
+	}
+	file, err = os.Open(path)
+	if err != nil {
+		return isil, file, err
+	}
+	return isil, file, nil
 }
 
 func main() {
@@ -47,32 +61,25 @@ func main() {
 	tagger := make(span.ISILTagger)
 
 	for _, s := range hfiles {
-		t, err := parseTagPath(s)
-		if err != nil {
-			log.Fatal(err)
-		}
-		file, err := os.Open(t.Path)
-		if err != nil {
-			log.Fatal(err)
-		}
+		isil, file, err := parseTagPath(s)
 		defer file.Close()
-		tagger[t.Tag], err = span.NewHoldingFilter(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		tagger[isil], err = span.NewHoldingFilter(file)
 		if err != nil && !*skip {
 			log.Fatal(err)
 		}
+
 	}
 
 	for _, s := range lfiles {
-		t, err := parseTagPath(s)
-		if err != nil {
-			log.Fatal(err)
-		}
-		file, err := os.Open(t.Path)
-		if err != nil {
-			log.Fatal(err)
-		}
+		isil, file, err := parseTagPath(s)
 		defer file.Close()
-		tagger[t.Tag], err = span.NewListFilter(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		tagger[isil], err = span.NewListFilter(file)
 		if err != nil && !*skip {
 			log.Fatal(err)
 		}
@@ -90,24 +97,4 @@ func main() {
 		fmt.Println(string(b))
 		os.Exit(0)
 	}
-
-	// filename := flag.Arg(0)
-	// file, err := os.Open(filename)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// lmap, errs := holdings.ParseHoldings(file)
-	// if len(errs) > 0 && !*skip {
-	// 	for _, e := range errs {
-	// 		log.Println(e)
-	// 	}
-	// 	log.Fatal("errors during processing")
-	// }
-
-	// b, err := json.Marshal(lmap)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println(string(b))
 }
