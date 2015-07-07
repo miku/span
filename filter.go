fd4fc7c6f5e12dc1d8576509fda5d09d56f89452
Martin Czygan <martin.czygan@gmail.com>
2015-07-07 16:43:23 +0200

    filter: update docs

--- filter.go
+++ filter.go
@@ -15,7 +15,8 @@ import (
 )
 
 // Filter wraps the decision, whether a given IntermediateSchema record should
-// be attached or not.
+// be attached or not. The decision may be based on record properties, simple
+// ISSN lists or more involved holdings files.
 type Filter interface {
 	Apply(finc.IntermediateSchema) bool
 }
@@ -41,17 +42,16 @@ func (f SourceFilter) MarshalJSON() ([]byte, error) {
 	return json.Marshal(f.SourceID)
 }
 
-// HoldingFilter decides ISIL-attachment by looking at licensing information
-// from OVID files. Ref is the reference date for moving wall calculations and
-// Table contains a map from ISSNs to licenses.
+// HoldingFilter looks at licensing information from an OVID files. Ref is the
+// reference date for moving wall calculations and Table contains a map from
+// ISSNs to licenses.
 type HoldingFilter struct {
 	Ref   time.Time
 	Table holdings.Licenses
 }
 
 // NewHoldingFilter loads the holdings information for a single institution.
-// Returns a single error, if errors has been encountered. The single errors
-// will be logger to stderr.
+// Returns a single error, if one or more errors have been encountered.
 func NewHoldingFilter(r io.Reader) (HoldingFilter, error) {
 	licenses, errs := holdings.ParseHoldings(r)
 	if len(errs) > 0 {
@@ -70,7 +70,8 @@ func (f HoldingFilter) MarshalJSON() ([]byte, error) {
 }
 
 // CoveredAndValid checks coverage and moving wall. If there is no entry for
-// an ISSN in the holdings file, we assume, there exists no valid license.
+// an ISSN in the holdings file, we assume, there is no valid license. The
+// given signature is a string, that encodes an items location in time.
 func (f HoldingFilter) CoveredAndValid(signature, issn string) bool {
 	licenses, ok := f.Table[issn]
 	if !ok {
@@ -87,8 +88,8 @@ func (f HoldingFilter) CoveredAndValid(signature, issn string) bool {
 	return false
 }
 
-// HoldingFilter compares the (year, volume, issue) of the
-// record with license information, including possible moving walls.
+// HoldingFilter compares the (year, volume, issue) of an intermediate schema
+// record with licensing information, including moving walls.
 func (f HoldingFilter) Apply(is finc.IntermediateSchema) bool {
 	signature := holdings.CombineDatum(fmt.Sprintf("%d", is.Date.Year()), is.Volume, is.Issue, "")
 	for _, issn := range append(is.ISSN, is.EISSN...) {
@@ -104,7 +105,7 @@ type ListFilter struct {
 	Set *container.StringSet
 }
 
-// NewAttachByList reads one record per line from reader.
+// NewAttachByList reads one record per line from reader. Empty lines are ignored.
 func NewListFilter(r io.Reader) (ListFilter, error) {
 	br := bufio.NewReader(r)
 	f := ListFilter{Set: container.NewStringSet()}
@@ -139,12 +140,13 @@ func (f ListFilter) Apply(is finc.IntermediateSchema) bool {
 	return false
 }
 
-// ISILTagger maps an ISIL to one or more Filters. If any of these filters
+// ISILTagger maps ISILs to one or more Filters. If any of these filters
 // return true, the ISIL shall be attached (therefore order of the filters
 // does not matter).
 type ISILTagger map[string][]Filter
 
-// Tags returns all ISILs that can be attached to a given intermediate schema record.
+// Tags returns all ISILs that could be attached to a given intermediate
+// schema record.
 func (t ISILTagger) Tags(is finc.IntermediateSchema) []string {
 	isils := container.NewStringSet()
 	for isil, filters := range t {
