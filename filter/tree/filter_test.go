0994e8b70407121fafedaeb0c0a477755ae9f964
Martin Czygan <martin.czygan@gmail.com>
2016-02-17 13:23:31 +0100

    filtertree: add not filter test

--- filter/tree/filter_test.go
+++ filter/tree/filter_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/miku/span/finc"
 )
 
+// TestOrFilter1 simple OR.
 func TestOrFilter1(t *testing.T) {
 	s := `
     {
@@ -47,6 +48,7 @@ func TestOrFilter1(t *testing.T) {
 	}
 }
 
+// TestOrFilter2 nested OR.
 func TestOrFilter2(t *testing.T) {
 	s := `
     {
@@ -96,6 +98,7 @@ func TestOrFilter2(t *testing.T) {
 	}
 }
 
+// TestAndFilter1 simple AND.
 func TestAndFilter1(t *testing.T) {
 	s := `
     {
@@ -122,6 +125,49 @@ func TestAndFilter1(t *testing.T) {
 		{finc.IntermediateSchema{SourceID: "1", MegaCollection: "A"}, true},
 		{finc.IntermediateSchema{SourceID: "2", MegaCollection: "A"}, false},
 		{finc.IntermediateSchema{SourceID: "2", MegaCollection: "C"}, false},
+	}
+
+	var tree FilterTree
+	if err := json.Unmarshal([]byte(s), &tree); err != nil {
+		t.Errorf("invalid filter: %s", err)
+	}
+	for _, test := range tests {
+		result := tree.Apply(test.record)
+		if result != test.result {
+			t.Errorf("Apply(%+v) got %v, want %v", test.record, result, test.result)
+		}
+	}
+}
+
+// TestNotFilter1 simple NOT.
+func TestNotFilter1(t *testing.T) {
+	s := `
+    {
+        "not": {
+            "and":[
+                {
+                    "source":[
+                        "1"
+                    ]
+                },
+                {
+                    "collection":[
+                        "A",
+                        "B"
+                    ]
+                }
+            ]
+        }
+    }
+    `
+	var tests = []struct {
+		record finc.IntermediateSchema
+		result bool
+	}{
+		{finc.IntermediateSchema{SourceID: "1", MegaCollection: "C"}, true},
+		{finc.IntermediateSchema{SourceID: "1", MegaCollection: "A"}, false},
+		{finc.IntermediateSchema{SourceID: "2", MegaCollection: "A"}, true},
+		{finc.IntermediateSchema{SourceID: "2", MegaCollection: "C"}, true},
 	}
 
 	var tree FilterTree
