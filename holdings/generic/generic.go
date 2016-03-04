c528afa9893b04912f47aefae3026afc12fc2211
Martin Czygan <martin.czygan@gmail.com>
2016-03-04 11:24:51 +0100

    readd holdings as subdir

---
+++ holdings/generic/generic.go
@@ -0,0 +1,92 @@
+package generic
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/miku/span/holdings"
+	"github.com/miku/span/holdings/google"
+	"github.com/miku/span/holdings/kbart"
+	"github.com/miku/span/holdings/ovid"
+)
+
+var ErrCannotSniffFormat = errors.New("cannot sniff holding file format")
+
+type ErrorList struct {
+	errors []error
+}
+
+func (e *ErrorList) Add(err error) {
+	e.errors = append(e.errors, err)
+}
+
+func (e ErrorList) Error() string {
+	var errs []string
+	for _, err := range e.errors {
+		errs = append(errs, err.Error())
+	}
+	return fmt.Sprintf("Errors: %v", strings.Join(errs, ", "))
+}
+
+// New returns a holding file to read entries from given a filename.
+func New(filename string) (holdings.File, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	return NewReader(bufio.NewReader(file))
+}
+
+// NewReader returns a holding file to read entries from. It will probe the
+// the different formats and use a suitable implementation.
+func NewReader(r io.Reader) (holdings.File, error) {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	var file holdings.File
+	var errors ErrorList
+
+	// probe kbart
+	file = kbart.NewReader(bytes.NewReader(b))
+	if entries, err := file.ReadEntries(); err != nil {
+		errors.Add(fmt.Errorf("kbart: %s", err))
+	} else {
+		if len(entries) > 0 {
+			log.Printf("holdings: kbart detected (%d)", len(entries))
+			return kbart.NewReader(bytes.NewReader(b)), nil
+		}
+	}
+
+	// probe ovid
+	file = ovid.NewReader(bytes.NewReader(b))
+	if entries, err := file.ReadEntries(); err != nil {
+		errors.Add(fmt.Errorf("ovid: %s", err))
+	} else {
+		if len(entries) > 0 {
+			log.Printf("holdings: ovid detected (%d)", len(entries))
+			return ovid.NewReader(bytes.NewReader(b)), nil
+		}
+	}
+
+	// probe google
+	file = google.NewReader(bytes.NewReader(b))
+	if entries, err := file.ReadEntries(); err != nil {
+		errors.Add(fmt.Errorf("google: %s", err))
+	} else {
+		if len(entries) > 0 {
+			log.Printf("holdings: google detected (%d)", len(entries))
+			return google.NewReader(bytes.NewReader(b)), nil
+		}
+	}
+
+	return file, errors
+}
---
+++ holdings/kbart/kbart.go
@@ -0,0 +1,251 @@
+package kbart
+
+import (
+	"bufio"
+	"errors"
+	"io"
+	"log"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/miku/span/holdings"
+)
+
+var (
+	ErrIncompleteLine     = errors.New("incomplete KBART line")
+	ErrIncompleteEmbargo  = errors.New("incomplete embargo")
+	ErrInvalidEmbargo     = errors.New("invalid embargo")
+	ErrMissingIdentifiers = errors.New("missing identifiers")
+)
+
+// delayPattern fixes allowed embargo strings.
+var delayPattern = regexp.MustCompile(`([P|R])([0-9]+)([Y|M|D])`)
+
+// embargo is a string representing a delay, e.g. P1Y, R10M.
+type embargo string
+
+// entry represents the various columns.
+type columns struct {
+	PublicationTitle         string
+	PrintIdentifier          string
+	OnlineIdentifier         string
+	FirstIssueDate           string
+	FirstVolume              string
+	FirstIssue               string
+	LastIssueDate            string
+	LastVolume               string
+	LastIssue                string
+	TitleURL                 string
+	FirstAuthor              string
+	TitleID                  string
+	Embargo                  embargo
+	CoverageDepth            string
+	CoverageNotes            string
+	PublisherName            string
+	InterlibraryRelevance    string
+	InterlibraryNationwide   string
+	InterlibraryTransmission string
+	InterlibraryComment      string
+	Publisher                string
+	Anchor                   string
+	ZDBID                    string
+}
+
+// Convert string like P12M, P1M, R10Y into a time.Duration.
+func (e embargo) AsDuration() (time.Duration, error) {
+	var d time.Duration
+
+	emb := strings.TrimSpace(string(e))
+	if len(emb) == 0 {
+		return d, nil
+	}
+
+	var parts = delayPattern.FindStringSubmatch(emb)
+
+	if len(parts) == 0 {
+		return d, nil
+	}
+
+	if len(parts) < 4 {
+		return d, ErrIncompleteEmbargo
+	}
+
+	i, err := strconv.Atoi(parts[2])
+	if err != nil {
+		return d, ErrInvalidEmbargo
+	}
+
+	switch parts[3] {
+	case "D":
+		return time.Duration(-i) * 24 * time.Hour, nil
+	case "M":
+		return time.Duration(-i) * 24 * time.Hour * 30, nil
+	case "Y":
+		return time.Duration(-i) * 24 * time.Hour * 30 * 365, nil
+	default:
+		return d, ErrInvalidEmbargo
+	}
+}
+
+// DisallowEarlier returns true if dates *before* the boundary should be
+// disallowed.
+func (e embargo) DisallowEarlier() bool {
+	return strings.HasPrefix(strings.TrimSpace(string(e)), "R")
+}
+
+// Reader reads tab-separated KBART. The encoding/csv package did not like
+// that particular format so we use a simple bufio.Reader for now.
+type Reader struct {
+	r          *bufio.Reader
+	currentRow int
+
+	SkipFirstRow           bool
+	SkipMissingIdentifiers bool
+	SkipIncompleteLines    bool
+	SkipInvalidEmbargo     bool
+}
+
+// NewReader creates a new KBART reader.
+func NewReader(r io.Reader) *Reader {
+	return &Reader{
+		SkipFirstRow:           true,
+		SkipMissingIdentifiers: true,
+		// some files have weird headers, refs. #7004
+		SkipIncompleteLines: true,
+		r:                   bufio.NewReader(r),
+	}
+}
+
+// ReadAll loads entries from a reader.
+func (r *Reader) ReadEntries() (holdings.Entries, error) {
+	entries := make(holdings.Entries)
+
+	for {
+		cols, entry, err := r.Read()
+
+		if err == io.EOF {
+			break
+		}
+
+		switch err {
+		case ErrMissingIdentifiers:
+			if r.SkipMissingIdentifiers {
+				log.Println("skipping line with missing identifiers")
+				continue
+			} else {
+				return entries, err
+			}
+		case ErrIncompleteLine:
+			if r.SkipIncompleteLines {
+				log.Println("skipping incomplete line")
+				continue
+			} else {
+				return entries, err
+			}
+		case ErrInvalidEmbargo:
+			if r.SkipInvalidEmbargo {
+				log.Println("skipping invalid embargo")
+				continue
+			} else {
+				return entries, err
+			}
+		}
+
+		pi := strings.TrimSpace(cols.PrintIdentifier)
+		oi := strings.TrimSpace(cols.OnlineIdentifier)
+
+		if pi == "" && oi == "" {
+			if !r.SkipMissingIdentifiers {
+				return entries, ErrMissingIdentifiers
+			}
+		}
+		if pi != "" {
+			entries[pi] = append(entries[pi], holdings.License(entry))
+		}
+		if oi != "" {
+			entries[oi] = append(entries[oi], holdings.License(entry))
+		}
+	}
+
+	return entries, nil
+}
+
+// Read reads a single line. Returns all columns, a parsed entry and error.
+func (r *Reader) Read() (columns, holdings.Entry, error) {
+	var entry holdings.Entry
+	var cols columns
+
+	if r.SkipFirstRow && r.currentRow == 0 {
+		if _, err := r.r.ReadString('\n'); err != nil {
+			return cols, entry, err
+		}
+	}
+	r.currentRow++
+
+	var line string
+	var err error
+
+	for {
+		line, err = r.r.ReadString('\n')
+		if strings.TrimSpace(line) != "" {
+			break
+		}
+		if err == io.EOF {
+			return cols, entry, io.EOF
+		}
+	}
+
+	record := strings.Split(line, "\t")
+
+	if err == io.EOF {
+		return cols, entry, io.EOF
+	}
+	if err != nil {
+		return cols, entry, err
+	}
+	// usually 23 columns, but might be only 19, refs. #7004
+	if len(record) < 23 {
+		log.Printf("warning: KBART line has %d columns, expected 23", len(record))
+	}
+
+	// if less than 13, bail out, refs. #7004
+	if len(record) < 13 {
+		return cols, entry, ErrIncompleteLine
+	}
+
+	cols = columns{
+		PrintIdentifier:  record[1],
+		OnlineIdentifier: record[2],
+		FirstIssueDate:   record[3],
+		FirstVolume:      record[4],
+		FirstIssue:       record[5],
+		LastIssueDate:    record[6],
+		LastVolume:       record[7],
+		LastIssue:        record[8],
+		Embargo:          embargo(record[12]),
+	}
+
+	emb, err := cols.Embargo.AsDuration()
+	if err != nil {
+		return cols, entry, err
+	}
+
+	entry = holdings.Entry{
+		Begin: holdings.Signature{
+			Date:   cols.FirstIssueDate,
+			Volume: cols.FirstVolume,
+			Issue:  cols.FirstIssue,
+		},
+		End: holdings.Signature{
+			Date:   cols.LastIssueDate,
+			Volume: cols.LastVolume,
+			Issue:  cols.LastIssue,
+		},
+		Embargo:                emb,
+		EmbargoDisallowEarlier: cols.Embargo.DisallowEarlier(),
+	}
+
+	return cols, entry, nil
+}
