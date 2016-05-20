e2ebe4197b2f7116afeebfdffa03fe4145372abe
Martin Czygan <martin.czygan@gmail.com>
2016-05-20 14:38:41 +0200

    thieme: extend nlm stub

--- sources/thieme/nlm.go
+++ sources/thieme/nlm.go
@@ -1,7 +1,11 @@
 package thieme
 
 import (
+	"encoding/base64"
 	"encoding/xml"
+	"fmt"
+	"strconv"
+	"time"
 
 	"github.com/miku/span/finc"
 )
@@ -14,13 +18,23 @@ type Article struct {
 	ArticleType               string `xml:"article-type,attr"`
 	Front                     struct {
 		JournalMeta struct {
-			JournalId         string   `xml:"journal-id"`
-			JournalTitleGroup string   `xml:"journal-title-group"`
-			Issn              []string `xml:"issn"`
-			Publisher         string   `xml:"publisher"`
+			JournalId         string `xml:"journal-id"`
+			JournalTitleGroup struct {
+				Title string `xml:"journal-title"`
+			} `xml:"journal-title-group"`
+			Issn      []string `xml:"issn"`
+			Publisher struct {
+				Name string `xml:"publisher-name"`
+			} `xml:"publisher"`
 		} `xml:"journal-meta"`
 		ArticleMeta struct {
-			ArticleId         string `xml:"article-id"`
+			Abstract struct {
+				Text string `xml:",innerxml"`
+			} `xml:"abstract"`
+			ArticleID []struct {
+				Value string `xml:",chardata"`
+				Type  string `xml:"pub-id-type,attr"`
+			} `xml:"article-id"`
 			ArticleCategories string `xml:"article-categories"`
 			TitleGroup        struct {
 				ArticleTitle    string `xml:"article-title"`
@@ -33,6 +47,7 @@ type Article struct {
 			PubDate      struct {
 				Month string `xml:"month"`
 				Year  string `xml:"year"`
+				Day   string `xml:"day"`
 			} `xml:"pub-date"`
 			Volume string `xml:"volume"`
 			Issue  string `xml:"issue"`
@@ -42,13 +57,72 @@ type Article struct {
 	} `xml:"front"`
 }
 
+func (article Article) DOI() string {
+	for _, id := range article.Front.ArticleMeta.ArticleID {
+		if id.Type == "doi" {
+			return id.Value
+		}
+	}
+	return ""
+}
+
+func (article Article) RecordID() string {
+	return fmt.Sprintf("ai-60-%s", base64.RawURLEncoding.EncodeToString([]byte(article.DOI())))
+}
+
+func (article Article) ParseTime() (time.Time, error) {
+	p := article.Front.ArticleMeta.PubDate
+	var year, month, day = "1970", "1", "1"
+	if p.Year != "" {
+		v, err := strconv.Atoi(p.Year)
+		if err == nil {
+			if v > 1000 && v < 3000 {
+				year = strconv.Itoa(v)
+			}
+		}
+	}
+	if p.Month != "" {
+		v, err := strconv.Atoi(p.Year)
+		if err == nil {
+			if v > 0 && v < 13 {
+				month = strconv.Itoa(v)
+			}
+		}
+	}
+	if p.Day != "" {
+		v, err := strconv.Atoi(p.Year)
+		if err == nil {
+			if v > 0 && v < 13 {
+				day = strconv.Itoa(v)
+			}
+		}
+	}
+	return time.Parse("2006-01-02", fmt.Sprintf("%04s-%02s-%02s", year, month, day))
+}
+
 func (article Article) ToIntermediateSchema() (*finc.IntermediateSchema, error) {
 	output := finc.NewIntermediateSchema()
+	output.RecordID = article.RecordID()
+	output.DOI = article.DOI()
+	if len(output.DOI) == 0 {
+		return output, fmt.Errorf("empty DOI")
+	}
+	output.URL = []string{fmt.Sprintf("http://doi.org/%s", output.DOI)}
+	output.Volume = article.Front.ArticleMeta.Volume
+	output.Issue = article.Front.ArticleMeta.Issue
 	output.ArticleTitle = article.Front.ArticleMeta.TitleGroup.ArticleTitle
-	output.JournalTitle = article.Front.JournalMeta.JournalTitleGroup
+	output.JournalTitle = article.Front.JournalMeta.JournalTitleGroup.Title
 	output.ISSN = article.Front.JournalMeta.Issn
-	if article.Front.JournalMeta.Publisher != "" {
-		output.Publishers = []string{article.Front.JournalMeta.Publisher}
+	output.StartPage = article.Front.ArticleMeta.Fpage
+	output.EndPage = article.Front.ArticleMeta.Lpage
+	output.Abstract = article.Front.ArticleMeta.Abstract.Text
+	t, err := article.ParseTime()
+	if err != nil {
+		return output, err
+	}
+	output.Date = t
+	if article.Front.JournalMeta.Publisher.Name != "" {
+		output.Publishers = []string{article.Front.JournalMeta.Publisher.Name}
 	}
 	return output, nil
 }
