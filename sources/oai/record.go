8ad168988d861bb4368ca5d0420897546c042fb2
Martin Czygan <martin.czygan@gmail.com>
2016-07-15 15:58:20 +0200

    a basic oai converter
    	$ span-import -i oai <(metha-cat http://export.arxiv.org/oai2) | head -1 | jq .
    	{
    	  "rft.atitle": "A determinant of Stirling cycle numbers counts unlabeled acyclic\n  single-source automata",
    	  "rft.date": "2007-03-30",
    	  "x.date": "2007-03-30T00:00:00Z",
    	  "abstract": "We show that a determinant of Stirling cycle numbers counts unlabeled acyclic\nsin...
    	  "authors": [
    	    {
    	      "rft.au": "Callan, David"
    	    }
    	  ],
    	  "url": [
    	    "http://arxiv.org/abs/0704.0004"
    	  ],
    	  "version": "0.9",
    	  "x.subjects": [
    	    "Mathematics - Combinatorics",
    	    "05A15"
    	  ]
    	}

--- sources/oai/record.go
+++ sources/oai/record.go
@@ -16,8 +16,8 @@ import (
 var datePattern = regexp.MustCompile(`[12][0-9][0-9][0-9]-[01][0-9]-[0123][0-9]`)
 
 type Record struct {
-	xml.Name `xml:"Record"`
-	Header   struct {
+	XMLName xml.Name `xml:"Record"`
+	Header  struct {
 		Status     string `xml:"status,attr"`
 		Identifier string `xml:"identifier"`
 		Datestamp  string `xml:"datestamp"`
@@ -25,15 +25,26 @@ type Record struct {
 	} `xml:"header"`
 	About string `xml:"about"`
 
-	Dc struct {
-		Title       string   `xml:"title"`
-		Creator     []string `xml:"creator"`
-		Subject     string   `xml:"subject"`
-		Description []string `xml:"description"`
-		Date        string   `xml:"date"`
-		Type        string   `xml:"type"`
-		Identifier  string   `xml:"identifier"`
-	} `xml:"dc"`
+	Metadata struct {
+		// http://www.openarchives.org/OAI/2.0/oai_dc.xsd
+		Dc struct {
+			Title       []string `xml:"title"`
+			Creator     []string `xml:"creator"`
+			Subject     []string `xml:"subject"`
+			Description []string `xml:"description"`
+			Publisher   []string `xml:"publisher"`
+			Contributor []string `xml:"contributor"`
+			Date        []string `xml:"date"`
+			Type        []string `xml:"type"`
+			Identifier  []string `xml:"identifier"`
+			Language    []string `xml:"language"`
+			Rights      []string `xml:"rights"`
+			Format      []string `xml:"format"`
+			Source      []string `xml:"source"`
+			Relation    []string `xml:"relation"`
+			Coverage    []string `xml:"coverage"`
+		} `xml:"dc"`
+	} `xml:"metadata"`
 }
 
 type DublinCore struct{}
@@ -48,18 +59,49 @@ func (s DublinCore) Iterate(r io.Reader) (<-chan []span.Importer, error) {
 }
 
 func (r *Record) Date() (time.Time, error) {
+	if len(r.Metadata.Dc.Date) == 0 {
+		return time.Time{}, fmt.Errorf("record has no date")
+	}
 	switch {
-	case len(r.Dc.Date) == 4:
-		return time.Parse("2006", r.Dc.Date)
-	case datePattern.FindString(r.Dc.Date) != "":
-		return time.Parse("2006-01-02", r.Dc.Date)
+	case len(r.Metadata.Dc.Date[0]) == 4:
+		return time.Parse("2006", r.Metadata.Dc.Date[0])
+	case datePattern.FindString(r.Metadata.Dc.Date[0]) != "":
+		return time.Parse("2006-01-02", r.Metadata.Dc.Date[0])
 	default:
-		return time.Time{}, fmt.Errorf("unknown date format: %s", r.Dc.Date)
+		return time.Time{}, fmt.Errorf("unknown date format: %s", r.Metadata.Dc.Date[0])
 	}
 }
 
 func (r *Record) Description() string {
-	return strings.Join(r.Dc.Description, "\n")
+	return strings.Join(r.Metadata.Dc.Description, "\n")
+}
+
+func (r *Record) Title() string {
+	if len(r.Metadata.Dc.Title) > 0 {
+		return r.Metadata.Dc.Title[0]
+	}
+	return ""
+}
+
+func (r *Record) Authors() []finc.Author {
+	var authors []finc.Author
+	for _, c := range r.Metadata.Dc.Creator {
+		authors = append(authors, finc.Author{Name: c})
+	}
+	for _, c := range r.Metadata.Dc.Contributor {
+		authors = append(authors, finc.Author{Name: c})
+	}
+	return authors
+}
+
+func (r *Record) Links() []string {
+	var links []string
+	for _, id := range r.Metadata.Dc.Identifier {
+		if strings.HasPrefix(id, "http") {
+			links = append(links, id)
+		}
+	}
+	return links
 }
 
 // ToInternalSchema converts a jats article into an internal schema.
@@ -74,27 +116,12 @@ func (r *Record) ToIntermediateSchema() (*finc.IntermediateSchema, error) {
 
 	output.Date = date
 	output.RawDate = output.Date.Format("2006-01-02")
-	output.Abstract = r.Description()
-
-	// output.Abstract = string(article.Front.Article.Abstract.Value)
-	// output.ArticleTitle = article.CombinedTitle()
-	// output.Authors = article.Authors()
-	// output.Fulltext = article.Body.Section.Value
-	// output.Genre = "article"
-	// output.RefType = "EJOUR"
-	// output.Headings = article.Headings()
-	// output.ISSN = article.ISSN()
-	// output.Issue = article.Front.Article.Issue.Value
-	// output.JournalTitle = article.JournalTitle()
-	// output.Languages = article.Languages()
-	// output.Publishers = append(output.Publishers, article.Front.Journal.Publisher.Name.Value)
-	// output.Subjects = article.Subjects()
-	// output.Volume = article.Front.Article.Volume.Value
-
-	// output.StartPage = article.Front.Article.FirstPage.Value
-	// output.EndPage = article.Front.Article.LastPage.Value
-	// output.PageCount = article.PageCount()
-	// output.Pages = fmt.Sprintf("%s-%s", output.StartPage, output.EndPage)
+	output.Abstract = strings.TrimSpace(r.Description())
+	output.ArticleTitle = r.Title()
+	output.Publishers = r.Metadata.Dc.Publisher
+	output.Authors = r.Authors()
+	output.URL = r.Links()
+	output.Subjects = r.Metadata.Dc.Subject
 
 	return output, nil
 }
