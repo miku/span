229f675ca5b9044cf7c7c96613f24d23df32f4c0
Martin Czygan <martin.czygan@gmail.com>
2016-10-26 14:08:16 +0200

    add preliminary formeta export, to test:
        $ span-export -o formeta file.is
    
    Where file.is is newline-delimited intermediate schema file.

---
+++ encoding/formeta/marshal.go
@@ -0,0 +1,75 @@
+package formeta
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"reflect"
+	"strings"
+	"time"
+
+	"github.com/miku/structs"
+)
+
+func escapeSingleQuote(s string) string {
+	return strings.Replace(s, "'", `\'`, -1)
+}
+
+func marshal(w io.Writer, k string, v interface{}) error {
+	kind := reflect.TypeOf(v).Kind()
+	switch kind {
+	case reflect.Struct:
+		// we want time.Time formatted, not the struct
+		switch t := v.(type) {
+		case time.Time:
+			if _, err := io.WriteString(w, fmt.Sprintf("%s: '%s', ", k, t.Format(time.RFC3339))); err != nil {
+				return err
+			}
+			return nil
+		}
+
+		if _, err := io.WriteString(w, k+" { "); err != nil {
+			return err
+		}
+
+		for _, f := range structs.New(v).Fields() {
+			if !f.IsExported() {
+				continue
+			}
+			if err := marshal(w, f.Name(), f.Value()); err != nil {
+				return err
+			}
+		}
+
+		if _, err := io.WriteString(w, " } "); err != nil {
+			return err
+		}
+	case reflect.Slice:
+		vv := reflect.ValueOf(v)
+		for i := 0; i < vv.Len(); i++ {
+			if err := marshal(w, k, vv.Index(i).Interface()); err != nil {
+				return err
+			}
+		}
+	case reflect.String:
+		_, err := io.WriteString(w, fmt.Sprintf("%s: '%v', ", k, escapeSingleQuote(v.(string))))
+		if err != nil {
+			return err
+		}
+	default:
+		_, err := io.WriteString(w, fmt.Sprintf("%s: '%v', ", k, v))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Marshal serializes a value as metafacture formeta. Partial implementation.
+func Marshal(v interface{}) ([]byte, error) {
+	buf := new(bytes.Buffer)
+	if err := marshal(buf, "", v); err != nil {
+		return []byte{}, err
+	}
+	return buf.Bytes(), nil
+}
--- finc/exporter/formeta.go
+++ finc/exporter/formeta.go
@@ -1,13 +1,12 @@
 package exporter
 
 import (
-	"fmt"
-
+	"github.com/miku/span/encoding/formeta"
 	"github.com/miku/span/finc"
 )
 
 type Formeta struct{}
 
 func (s *Formeta) Export(is finc.IntermediateSchema, _ bool) ([]byte, error) {
-	return []byte{}, fmt.Errorf("not yet implemented")
+	return formeta.Marshal(is)
 }
