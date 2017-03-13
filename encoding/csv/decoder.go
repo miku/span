761deca593cb014f370c12648ed137b3e940ab29
Martin Czygan <martin.czygan@gmail.com>
2017-03-13 16:21:11 +0100

    csv: add struct decoder

---
+++ encoding/csv/decoder.go
@@ -0,0 +1,60 @@
+package csv
+
+import (
+	stdcsv "encoding/csv"
+	"reflect"
+
+	"github.com/miku/structs"
+)
+
+// A Decoder reads and decodes CSV rows from an input stream.
+type Decoder struct {
+	r       *stdcsv.Reader // The underlying reader.
+	Header  []string       // Column names.
+	started bool           // Whether reading has started.
+}
+
+// NewDecoder returns a new decoder.
+func NewDecoder(c *stdcsv.Reader) *Decoder {
+	return &Decoder{r: c}
+}
+
+func (dec *Decoder) readHeader() error {
+	if dec.started {
+		return nil
+	}
+	record, err := dec.r.Read()
+	if err != nil {
+		return err
+	}
+	dec.Header, dec.started = record, true
+	return nil
+}
+
+// Decode a single entry, use csv struct tags.
+func (dec *Decoder) Decode(v interface{}) error {
+	if err := dec.readHeader(); err != nil {
+		return err
+	}
+	if reflect.TypeOf(v).Elem().Kind() != reflect.Struct {
+		return nil
+	}
+	record, err := dec.r.Read()
+	if err != nil {
+		return err
+	}
+	s := structs.New(v)
+	for _, f := range s.Fields() {
+		name := f.Tag("csv")
+		for i, h := range dec.Header {
+			if name != h {
+				continue
+			}
+			if err := f.Set(record[i]); err != nil {
+				return err
+			}
+			break
+		}
+	}
+	return nil
+}
