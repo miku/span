34f59009c0a303fbae056a03d24800e27e8f6e53
Martin Czygan <martin.czygan@gmail.com>
2017-03-14 14:57:25 +0100

    add tsv: simplified tabular decoder

---
+++ encoding/tsv/decoder.go
@@ -0,0 +1,76 @@
+package tsv
+
+import (
+	"bufio"
+	"io"
+	"reflect"
+	"sync"
+
+	"strings"
+
+	"github.com/fatih/structs"
+	"github.com/miku/span"
+)
+
+// A Decoder reads and decodes TSV rows from an input stream.
+type Decoder struct {
+	Header []string         // Column names.
+	r      *span.SkipReader // The underlying buffered reader.
+	once   sync.Once
+}
+
+// NewDecoder returns a new decoder.
+func NewDecoder(r io.Reader) *Decoder {
+	return &Decoder{r: span.NewSkipReader(bufio.NewReader(r))}
+}
+
+// readHeader attempts to read the first row and store the column names. If the
+// header has been already set manually, the values won't be overwritten.
+func (dec *Decoder) readHeader() (err error) {
+	dec.once.Do(func() {
+		if len(dec.Header) > 0 {
+			return
+		}
+		var line string
+		if line, err = dec.r.ReadString('\n'); err != nil {
+			return
+		}
+		dec.Header = strings.Split(line, "\t")
+	})
+	return
+}
+
+// Decode a single entry, use csv struct tags.
+func (dec *Decoder) Decode(v interface{}) error {
+	if err := dec.readHeader(); err != nil {
+		return err
+	}
+	if reflect.TypeOf(v).Elem().Kind() != reflect.Struct {
+		return nil
+	}
+	line, err := dec.r.ReadString('\n')
+	if err == io.EOF {
+		return io.EOF
+	}
+	record := strings.Split(line, "\t")
+
+	s := structs.New(v)
+	for _, f := range s.Fields() {
+		tag := f.Tag("csv")
+		if tag == "" || tag == "-" {
+			continue
+		}
+		for i, header := range dec.Header {
+			if tag != header {
+				continue
+			}
+			if i >= len(record) {
+				break // Record has too few columns.
+			}
+			if err := f.Set(record[i]); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
---
+++ encoding/tsv/decoder_test.go
@@ -0,0 +1,134 @@
+package tsv
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+var (
+	testOne = `
+publication_title	print_identifier
+Hello	123
+`
+
+	testTwo = `
+publication_title	print_identifier	online_identifier	date_first_issue_online	num_first_vol_online	num_first_issue_online	date_last_issue_online	num_last_vol_online	num_last_issue_online	title_url	first_author	title_id	embargo_info	coverage_depth	coverage_notes	publisher_name	own_anchor	package:collection	il_relevance	il_nationwide	il_electronic_transmission	il_comment	all_issns	zdb_id
+ Bill of Rights Journal (via Hein Online)	0006-2499		1968	1		1996	29		http://heinonline.org/HOL/Index?index=journals/blorij&collection=journals		227801		Volltext		via Hein Online		HEIN:hein	Keine Fernleihe				0006-2499	2805467-2
+`
+)
+
+type TestSimple struct {
+	Title string `csv:"publication_title"`
+	ID    string `csv:"print_identifier"`
+}
+
+type TestRepetition struct {
+	Title       string `csv:"publication_title"`
+	Publication string `csv:"publication_title"`
+	ID          string `csv:"print_identifier"`
+}
+
+type TestEntry struct {
+	PublicationTitle                   string `csv:"publication_title"`
+	PrintIdentifier                    string `csv:"print_identifier"`
+	OnlineIdentifier                   string `csv:"online_identifier"`
+	FirstIssueDate                     string `csv:"date_first_issue_online"`
+	FirstVolume                        string `csv:"num_first_vol_online"`
+	FirstIssue                         string `csv:"num_first_issue_online"`
+	LastIssueDate                      string `csv:"date_last_issue_online"`
+	LastVolume                         string `csv:"num_last_vol_online"`
+	LastIssue                          string `csv:"num_last_issue_online"`
+	TitleURL                           string `csv:"title_url"`
+	FirstAuthor                        string `csv:"first_author"`
+	TitleID                            string `csv:"title_id"`
+	Embargo                            string `csv:"embargo_info"`
+	CoverageDepth                      string `csv:"coverage_depth"`
+	CoverageNotes                      string `csv:"coverage_notes"`
+	PublisherName                      string `csv:"publisher_name"`
+	OwnAnchor                          string `csv:"own_anchor"`
+	PackageCollection                  string `csv:"package:collection"`
+	InterlibraryRelevance              string `csv:"il_relevance"`
+	InterlibraryNationwide             string `csv:"il_nationwide"`
+	InterlibraryElectronicTransmission string `csv:"il_electronic_transmission"`
+	InterlibraryComment                string `csv:"il_comment"`
+	AllSerialNumbers                   string `csv:"all_issns"`
+	ZDBID                              string `csv:"zdb_id"`
+	Location                           string `csv:"location"`
+	TitleNotes                         string `csv:"title_notes"`
+	StaffNotes                         string `csv:"staff_notes"`
+	VendorID                           string `csv:"vendor_id"`
+	OCLCCollectionName                 string `csv:"oclc_collection_name"`
+	OCLCCollectionID                   string `csv:"oclc_collection_id"`
+	OCLCEntryID                        string `csv:"oclc_entry_id"`
+	OCLCLinkScheme                     string `csv:"oclc_link_scheme"`
+	OCLCNumber                         string `csv:"oclc_number"`
+	Action                             string `csv:"ACTION"`
+}
+
+func TestDecode(t *testing.T) {
+	dec := NewDecoder(strings.NewReader(testOne))
+
+	var example TestSimple
+	if err := dec.Decode(&example); err != nil {
+		t.Errorf(err.Error())
+	}
+
+	expected := TestSimple{Title: "Hello", ID: "123"}
+	if !reflect.DeepEqual(example, expected) {
+		t.Errorf("Decode: got %#v, want %#v", example, expected)
+	}
+}
+
+func TestDecodeRepetitions(t *testing.T) {
+	dec := NewDecoder(strings.NewReader(testOne))
+
+	var example TestRepetition
+	if err := dec.Decode(&example); err != nil {
+		t.Errorf(err.Error())
+	}
+
+	expected := TestRepetition{Title: "Hello", Publication: "Hello", ID: "123"}
+	if !reflect.DeepEqual(example, expected) {
+		t.Errorf("Decode: got %#v, want %#v", example, expected)
+	}
+}
+
+func TestDecodeKbart(t *testing.T) {
+	dec := NewDecoder(strings.NewReader(testTwo))
+
+	var example TestEntry
+	if err := dec.Decode(&example); err != nil {
+		t.Errorf(err.Error())
+	}
+
+	expected := TestEntry{
+		PublicationTitle:                   "Bill of Rights Journal (via Hein Online)",
+		PrintIdentifier:                    "0006-2499",
+		OnlineIdentifier:                   "",
+		FirstIssueDate:                     "1968",
+		FirstVolume:                        "1",
+		FirstIssue:                         "",
+		LastIssueDate:                      "1996",
+		LastVolume:                         "29",
+		LastIssue:                          "",
+		TitleURL:                           "http://heinonline.org/HOL/Index?index=journals/blorij&collection=journals",
+		FirstAuthor:                        "",
+		TitleID:                            "227801",
+		Embargo:                            "",
+		CoverageDepth:                      "Volltext",
+		CoverageNotes:                      "",
+		PublisherName:                      "via Hein Online",
+		OwnAnchor:                          "",
+		PackageCollection:                  "HEIN:hein",
+		InterlibraryRelevance:              "Keine Fernleihe",
+		InterlibraryNationwide:             "",
+		InterlibraryElectronicTransmission: "",
+		InterlibraryComment:                "",
+		AllSerialNumbers:                   "0006-2499",
+		ZDBID:                              "2805467-2",
+	}
+	if !reflect.DeepEqual(example, expected) {
+		t.Errorf("Decode: got %#v, want %#v", example, expected)
+	}
+}
