f28f6d08f7a60653dc583ed4065da2e9a1e0876e
Martin Czygan <martin.czygan@gmail.com>
2017-03-15 18:16:08 +0100

    kbart: update tests

--- licensing/kbart/holdings_test.go
+++ licensing/kbart/holdings_test.go
@@ -3,27 +3,37 @@ package kbart
 import (
 	"bufio"
 	"os"
+	"regexp"
+	"strings"
 	"testing"
-	"time"
+
+	"github.com/miku/span/container"
+	"github.com/miku/span/licensing"
 )
 
 var fixture = "../../fixtures/kbart.txt"
 
-func TestByISSN(t *testing.T) {
-	started := time.Now()
+func loadHoldings() (*Holdings, error) {
 	if _, err := os.Stat(fixture); os.IsNotExist(err) {
-		t.Skipf("skipping, since fixture not found: %s", fixture)
+		return nil, err
 	}
 	file, err := os.Open(fixture)
 	if err != nil {
-		t.Fatalf(err.Error())
+		return nil, err
 	}
 	defer file.Close()
 	holdings := new(Holdings)
 	if _, err := holdings.ReadFrom(bufio.NewReader(file)); err != nil {
+		return nil, err
+	}
+	return holdings, nil
+}
+
+func TestByISSN(t *testing.T) {
+	holdings, err := loadHoldings()
+	if err != nil {
 		t.Fatalf(err.Error())
 	}
-	t.Logf("loading took: %s", time.Since(started))
 	entries := holdings.ByISSN("2079-8245")
 	if len(entries) != 1 {
 		t.Errorf("ByISSN: got %v, want 1", len(entries))
@@ -33,20 +43,41 @@ func TestByISSN(t *testing.T) {
 }
 
 func BenchmarkByISSN(b *testing.B) {
-	if _, err := os.Stat(fixture); os.IsNotExist(err) {
-		b.Skipf("skipping, since fixture not found: %s", fixture)
-	}
-	file, err := os.Open(fixture)
+	holdings, err := loadHoldings()
 	if err != nil {
-		b.Fatalf(err.Error())
-	}
-	defer file.Close()
-	holdings := new(Holdings)
-	if _, err := holdings.ReadFrom(bufio.NewReader(file)); err != nil {
 		b.Fatalf(err.Error())
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		holdings.ByISSN("2079-8245")
+	}
+}
+
+func TestFilter(t *testing.T) {
+	holdings, err := loadHoldings()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	// Test filter.
+	entries := holdings.Filter(func(e licensing.Entry) bool {
+		return strings.Contains(strings.ToLower(e.TitleURL), "wiso")
+	})
+	if len(entries) != 702 {
+		t.Errorf("Filter: got %v, want %v", len(entries), 702)
+	}
+
+	// Test database name extraction.
+	p := regexp.MustCompile(`[A-Z]{3,4}`)
+	names := container.NewStringSet()
+	for _, e := range entries {
+		matches := p.FindAllString(e.TitleURL, -1)
+		for _, m := range matches {
+			names.Add(m)
+		}
+	}
+
+	if len(names.Values()) != 534 {
+		t.Errorf("Filter: got %v, want %v", len(names.Values()), 534)
 	}
 }
