923244d6824293d3801faf11176348543323d003
Martin Czygan <martin.czygan@gmail.com>
2017-05-03 11:21:07 +0200

    v0.1.150

--- common.go
+++ common.go
@@ -41,7 +41,7 @@ import (
 
 const (
 	// AppVersion of span package. Commandline tools will show this on -v.
-	AppVersion = "0.1.149"
+	AppVersion = "0.1.150"
 	// KeyLengthLimit is a limit imposed by memcached protocol, which is used
 	// for blob storage as of June 2015. If we change the key value store,
 	// this limit might become obsolete.
