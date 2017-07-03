dcd2b7f8f8e479f9968da5aada99bbe9b837fa4d
Martin Czygan <martin.czygan@gmail.com>
2017-07-03 16:22:18 +0200

    span-unify: two example inputs

--- cmd/span-unify/main.go
+++ cmd/span-unify/main.go
@@ -2,13 +2,20 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"log"
 	"os"
 
+	"github.com/miku/span/finc"
 	"github.com/miku/span/s"
 	"github.com/miku/xmlstream"
 )
+
+// IntermediateSchemaer wrap a basic conversion method.
+type IntermediateSchemaer interface {
+	ToIntermediateSchema() (*finc.IntermediateSchema, error)
+}
 
 func main() {
 	formatName := flag.String("i", "", "input format name")
@@ -22,12 +29,24 @@ func main() {
 	switch *formatName {
 	case "highwire":
 		scanner = xmlstream.NewScanner(os.Stdin, new(s.Record))
+	case "ceeol":
+		scanner = xmlstream.NewScanner(os.Stdin, new(s.Article))
 	default:
 		log.Fatalf("unknown format: %s", *formatName)
 	}
 
 	for scanner.Scan() {
 		tag := scanner.Element()
-		log.Println(tag)
+		converter, ok := tag.(IntermediateSchemaer)
+		if !ok {
+			continue
+		}
+		output, err := converter.ToIntermediateSchema()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := json.NewEncoder(os.Stdout).Encode(output); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
