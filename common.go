4b978a7019dfc96a3120aca067344b6805d2f0d3
Martin Czygan <martin.czygan@gmail.com>
2017-07-08 17:47:24 +0200

    cleanup code

--- common.go
+++ common.go
@@ -22,11 +22,8 @@
 package span
 
 import (
-	"bufio"
 	"fmt"
 	"html"
-	"io"
-	"log"
 	"regexp"
 	"strings"
 
@@ -60,24 +57,6 @@ func (s Skip) Error() string {
 // UnescapeTrim unescapes HTML character references and trims the space of a given string.
 func UnescapeTrim(s string) string {
 	return strings.TrimSpace(html.UnescapeString(s))
-}
-
-// ByteSink is a fan in writer for a []byte channel.
-// A newline is appended after each object.
-func ByteSink(w io.Writer, out chan []byte, done chan bool) {
-	f := bufio.NewWriter(w)
-	for b := range out {
-		if _, err := f.Write(b[:]); err != nil {
-			log.Fatal(err)
-		}
-		if _, err := f.Write([]byte("\n")); err != nil {
-			log.Fatal(err)
-		}
-	}
-	if err := f.Flush(); err != nil {
-		log.Fatal(err)
-	}
-	done <- true
 }
 
 // DetectLang3 returns the best guess 3-letter language code for a given text.
