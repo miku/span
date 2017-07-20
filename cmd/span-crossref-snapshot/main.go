e9c9e40158c759f608437ffe6edd51ee3863df81
Martin Czygan <martin.czygan@gmail.com>
2017-07-20 22:01:58 +0200

    scs: use a buffer

--- cmd/span-crossref-snapshot/main.go
+++ cmd/span-crossref-snapshot/main.go
@@ -11,13 +11,36 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
+	"strings"
 
 	"bufio"
 
+	"github.com/miku/clam"
 	"github.com/miku/parallel"
 	"github.com/miku/span/formats/crossref"
 )
+
+const tmpPrefix = "span-crossref-snapshot-"
+
+// line is a list of fields.
+type line struct {
+	cols []string
+}
+
+// MarshalText returns a tab-separated columns.
+func (l line) MarshalText() ([]byte, error) {
+	return []byte(fmt.Sprintf("%s\n", strings.Join(l.cols, "\t"))), nil
+}
+
+// WriteTo implements io.WriterTo.
+func (l line) WriteTo(w io.Writer) (int64, error) {
+	b, err := l.MarshalText()
+	if err != nil {
+		return 0, err
+	}
+	n, err := w.Write(b)
+	return int64(n), err
+}
 
 func main() {
 	// First the filename, DOI and deposited date are extracted into a temporary file.
@@ -28,9 +51,11 @@ func main() {
 	// For each file (sha), keep the extracted list compressed and cached at
 	// ~/.cache/span-crossref-snapshot/. Also, keep a result cache for a set of files.
 
+	batchSize := flag.Int("b", 10, "batch size")
+
 	flag.Parse()
 
-	f, err := ioutil.TempFile("", "span-crossref-snapshot-")
+	f, err := ioutil.TempFile("", tmpPrefix)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -41,6 +66,7 @@ func main() {
 
 	for _, filename := range flag.Args() {
 		log.Println(filename)
+
 		f, err := os.Open(filename)
 		if err != nil {
 			log.Fatal(err)
@@ -48,30 +74,30 @@ func main() {
 		br := bufio.NewReader(f)
 
 		// Close over filename, so we can safely use it with goroutines.
-		var createProcessor = func(filename string) *parallel.Processor {
+		var setupProcessor = func(filename string) *parallel.Processor {
 			p := parallel.NewProcessor(br, w, func(b []byte) ([]byte, error) {
 				var resp crossref.BulkResponse
 				if err := json.Unmarshal(b, &resp); err != nil {
 					return nil, err
 				}
-				var items [][]byte
+				var buf bytes.Buffer
 				for _, doc := range resp.Message.Items {
 					date, err := doc.Deposited.Date()
 					if err != nil {
 						return nil, err
 					}
-					s := fmt.Sprintf("%s\t%s\t%s", filename, date.Format("2006-01-02"), doc.DOI)
-					items = append(items, []byte(s))
+					l := line{[]string{filename, date.Format("2006-01-02"), doc.DOI}}
+					if _, err := l.WriteTo(&buf); err != nil {
+						return nil, err
+					}
 				}
-				return bytes.Join(items, []byte("\n")), nil
+				return buf.Bytes(), nil
 			})
+			p.BatchSize = *batchSize
 			return p
 		}
 
-		// Create, configure, run.
-		p := createProcessor(filename)
-		p.BatchSize = 5 // Each item might be large.
-		if err := p.Run(); err != nil {
+		if err := setupProcessor(filename).Run(); err != nil {
 			log.Fatal(err)
 		}
 		if err := f.Close(); err != nil {
@@ -82,24 +108,16 @@ func main() {
 	if err := w.Flush(); err != nil {
 		log.Fatal(err)
 	}
-
-	g, err := ioutil.TempFile("", "span-crossref-snapshot-")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer g.Close()
-
-	log.Println("Rewinding.")
 	if _, err := f.Seek(0, io.SeekStart); err != nil {
 		log.Fatal(err)
 	}
 
 	log.Println("Sorting.")
-	cmd := exec.Command("sort", "-S25%", "-k3,3", "-k2,2", "-u")
-	cmd.Stdin = f
-	cmd.Stdout = g
-	if err := cmd.Run(); err != nil {
+
+	output, err := clam.RunOutput("LC_ALL=C sort -S25% -k3,3 -k2,2 {{ input }} | LC_ALL=C sort -S25% -k3,3 -u > {{ output }}",
+		clam.Map{"input": f.Name()})
+	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(g.Name())
+	log.Println(output)
 }
