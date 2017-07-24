36cdf76a2c7fa50648e37bb71450977b594b1d7b
Martin Czygan <martin.czygan@gmail.com>
2017-07-24 16:19:50 +0200

    scs: use a single input file
    Input: single file of concatenated crossref API message items.

--- cmd/span-crossref-snapshot/main.go
+++ cmd/span-crossref-snapshot/main.go
@@ -1,5 +1,5 @@
-// Given a set of API responses from crossref, generate a file that contains only
-// the latest version of a record, determined by DOI and deposit date.
+// Given as single file with crossref works API message, create a potentially
+// smaller file, which contains only the most recent version of each document.
 package main
 
 import (
@@ -11,157 +11,69 @@ import (
 	"io"
 	"log"
 	"os"
-	"path/filepath"
-	"runtime"
 	"strings"
 
-	"io/ioutil"
+	gzip "github.com/klauspost/pgzip"
 
-	"github.com/miku/clam"
 	"github.com/miku/span/formats/crossref"
 	"github.com/miku/span/parallel"
-	"github.com/reusee/mmh3"
 )
-
-const (
-	tmpPrefix = "span-crossref-snapshot-"
-)
-
-var (
-	batchSize = flag.Int("b", 20, "batch size")
-	cacheBase = filepath.Join(UserHomeDir(), ".cache/span-crossref-snapshot")
-)
-
-// UserHomeDir returns the home of the user.
-func UserHomeDir() string {
-	if runtime.GOOS == "windows" {
-		home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
-		if home == "" {
-			home = os.Getenv("USERPROFILE")
-		}
-		return home
-	}
-	return os.Getenv("HOME")
-}
-
-// HashReader returns the sha256 hex digest of the given reader.
-func HashReader(r io.ReadSeeker) (hexdigest string, err error) {
-	hasher := mmh3.New32()
-	if _, err = io.Copy(hasher, r); err != nil {
-		return
-	}
-	if _, err = r.Seek(0, io.SeekStart); err != nil {
-		return
-	}
-	return fmt.Sprintf("mmh3-%x-batchsize-%d", hasher.Sum(nil), *batchSize), nil
-}
 
 // WriteFields writes a variable number of fields as tab separated values into a writer.
-func WriteFields(w io.Writer, s ...string) (int, error) {
+func WriteFields(w io.Writer, values ...interface{}) (int, error) {
+	var s []string
+	for _, v := range values {
+		s = append(s, fmt.Sprintf("%v", v))
+	}
 	return io.WriteString(w, fmt.Sprintf("%s\n", strings.Join(s, "\t")))
 }
 
-// SetupProcessor creates a new processor. Here, we extract the interesting bits
-// from the given file and write them as tabular values into the given writer.
-func SetupProcessor(f *os.File, w io.Writer) *parallel.Processor {
-	// reduceDocs is our transformation function.
-	reduceDocs := func(i int64, b []byte) ([]byte, error) {
-		var resp crossref.BulkResponse
-		if err := json.Unmarshal(b, &resp); err != nil {
-			return nil, err
-		}
-		var buf bytes.Buffer
-		for j, doc := range resp.Message.Items {
-			date, err := doc.Deposited.Date()
-			if err != nil {
-				return nil, err
-			}
-			isodate := date.Format("2006-01-02")
-			if _, err := WriteFields(&buf, f.Name(), isodate, doc.DOI, fmt.Sprintf("%d", i), fmt.Sprintf("%d", j)); err != nil {
-				return nil, err
-			}
-		}
-		return buf.Bytes(), nil
-	}
-	p := parallel.NewProcessor(bufio.NewReader(f), w, reduceDocs)
-	p.BatchSize = *batchSize
-	return p
-}
-
 func main() {
-	// First the filename, DOI and deposited date are extracted into a temporary file.
-	// This file is sorted by DOI and deposited date, only the latest date is kept.
-	// Then, for each file extract only the newest records (must keep a list of DOI in
-	// memory or maybe in an embedded key value store, say bolt).
-	// ...
-	// For each file (sha), keep the extracted list compressed and cached at
-	// ~/.cache/span-crossref-snapshot/. Also, keep a result cache for a set of files.
+	compressed := flag.Bool("z", false, "input is gzip compressed")
 
 	flag.Parse()
 
-	// Setup cache directory.
-	cacheDir := filepath.Join(cacheBase, "tables")
-	if _, err := os.Stat(cacheDir); err != nil {
-		if os.IsNotExist(err) {
-			if err := os.MkdirAll(cacheDir, 0755); err != nil {
-				log.Fatal(err)
-			}
-		} else {
-			log.Fatal(err)
-		}
+	if flag.NArg() == 0 {
+		log.Fatal("input file required")
 	}
-	log.Println(cacheDir)
+	var reader io.Reader
 
-	// Collect filenames of extracted files here.
-	var files []string
-
-	for _, filename := range flag.Args() {
-
-		log.Printf(filename)
-
-		f, err := os.Open(filename)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer f.Close()
-
-		hexdigest, err := HashReader(f)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println(hexdigest)
-
-		cacheFile := filepath.Join(cacheDir, hexdigest)
-
-		if _, err := os.Stat(cacheFile); err != nil {
-			if os.IsNotExist(err) {
-				tf, err := ioutil.TempFile("", tmpPrefix)
-				if err != nil {
-					log.Fatal(err)
-				}
-				bw := bufio.NewWriter(tf)
-				if err := SetupProcessor(f, bw).Run(); err != nil {
-					log.Fatal(err)
-				}
-				if err := bw.Flush(); err != nil {
-					log.Fatal(err)
-				}
-				if err := os.Rename(tf.Name(), cacheFile); err != nil {
-					log.Fatal(err)
-				}
-			}
-		}
-		files = append(files, cacheFile)
-	}
-
-	// Concatenate files. Sort by DOI (3), then date reversed (2); then unique by DOI
-	// (3). Should keep the entry of the last update (filename, document date, DOI).
-	t := "cat {{ files }} | LC_ALL=C sort -S25% -k3,3 -rk2,2 | LC_ALL=C sort -S25% -k3,3 -u > {{ output }}"
-
-	output, err := clam.RunOutput(t, clam.Map{"files": strings.Join(files, " ")})
+	f, err := os.Open(flag.Arg(0))
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer f.Close()
+	reader = f
 
-	fmt.Println(output)
+	if *compressed {
+		g, err := gzip.NewReader(f)
+		if err != nil {
+			log.Fatal(err)
+		}
+		reader = g
+	}
+
+	br := bufio.NewReader(reader)
+	bw := bufio.NewWriter(os.Stdout)
+	defer bw.Flush()
+
+	processor := parallel.NewProcessor(br, bw, func(lineno int64, b []byte) ([]byte, error) {
+		var doc crossref.Document
+		if err := json.Unmarshal(b, &doc); err != nil {
+			return nil, err
+		}
+		date, err := doc.Deposited.Date()
+		if err != nil {
+			return nil, err
+		}
+		var buf bytes.Buffer
+		if _, err := WriteFields(&buf, lineno, date.Format("2006-01-02"), doc.DOI); err != nil {
+			return nil, err
+		}
+		return buf.Bytes(), nil
+	})
+	processor.BatchSize = 100000
+	if err := processor.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
