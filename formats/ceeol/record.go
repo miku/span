4d9d6985aba8a09b3dc20226dba5bff55d732f27
Martin Czygan <martin.czygan@gmail.com>
2018-11-07 15:59:46 +0100

    wip: ceeol: append a few fields

--- formats/ceeol/record.go
+++ formats/ceeol/record.go
@@ -123,6 +123,71 @@ func (r Record) ID() (string, error) {
 	return "", fmt.Errorf("missing identifier")
 }
 
+// Title returns some title.
+func (r Record) Title() (string, error) {
+	title, err := r.GetFirstDataField("245.a")
+	if err != nil {
+		return "", err
+	}
+	subtitle, err := r.GetFirstDataField("245.b")
+	if err != nil {
+		return "", err
+	}
+	title = strings.TrimSpace(title)
+	subtitle = strings.TrimSpace(subtitle)
+	if subtitle == "" {
+		return title, nil
+	}
+	return fmt.Sprintf("%s: %s", title, subtitle), nil
+}
+
+// RecordFormat returns "book", "article" or "unknown".
+func (r Record) RecordFormat() string {
+	v, _ := r.GetDataFields("022.a")
+	if len(v) > 0 {
+		return "article"
+	}
+	v, _ = r.GetDataFields("020.a")
+	if len(v) > 0 {
+		return "book"
+	}
+	return "unknown"
+}
+
+// ISSN returns a list of ISSN.
+func (r Record) ISSN() (values []string) {
+	values, _ = r.GetDataFields("022.a")
+	return values
+}
+
+// ISBN returns a list of ISBN.
+func (r Record) ISBN() (values []string) {
+	values, _ = r.GetDataFields("020.a")
+	return values
+}
+
+func (r Record) Abstract() string {
+	values, _ := r.GetDataFields("520.a")
+	return strings.Join(values, "\n")
+}
+
+func (r Record) Authors() (authors []finc.Author) {
+	values, _ := r.GetDataFields("100.a")
+	for _, value := range values {
+		authors = append(authors, finc.Author{Name: value})
+	}
+	values, _ = r.GetDataFields("700.a")
+	for _, value := range values {
+		authors = append(authors, finc.Author{Name: value})
+	}
+	return authors
+}
+
+func (r Record) SubjectHeadings() []string {
+	values, _ := r.GetDataFields("650.a")
+	return values
+}
+
 // ToIntermediateSchema converts CEEOL marcxml data into intermediate schema.
 func (r Record) ToIntermediateSchema() (*finc.IntermediateSchema, error) {
 	output := finc.NewIntermediateSchema()
@@ -131,11 +196,30 @@ func (r Record) ToIntermediateSchema() (*finc.IntermediateSchema, error) {
 		return output, span.Skip{Reason: err.Error()}
 	}
 	output.RecordID = v
+
+	v, err = r.Title()
+	if err != nil {
+		return output, span.Skip{
+			Reason: fmt.Sprintf("missing title: %s", output.RecordID)}
+	}
+
+	switch r.RecordFormat() {
+	case "book":
+		output.BookTitle = v
+	default:
+		output.ArticleTitle = v
+	}
+
 	output.Format = Format
 	output.SourceID = SourceIdentifier
 	output.Genre = Genre
 	output.RefType = DefaultRefType
 	output.MegaCollections = []string{Collection}
+	output.ISSN = r.ISSN()
+	output.ISBN = r.ISBN()
+	output.Abstract = r.Abstract()
+	output.Authors = r.Authors()
+	output.Subjects = r.SubjectHeadings()
 
 	return output, nil
 }
