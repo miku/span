5d6ee9aab8a1fd8b4e35ccc2f026ae6e91a78588
Martin Czygan <martin.czygan@gmail.com>
2019-01-28 15:02:23 +0100

    span-amsl: match by collection name and isil

--- cmd/span-amsl/main.go
+++ cmd/span-amsl/main.go
@@ -156,7 +156,7 @@ func main() {
 
 		// Holding files concatenated.
 		for _, other := range hfc {
-			if !doc.MatchByKey(other, "megaCollection") {
+			if !doc.MatchByKey(other, "megaCollection") && !doc.MatchByKey(other, "ISIL") {
 				continue
 			}
 			// Update from matching document.
