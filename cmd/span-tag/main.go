4209c18414424eb34c62e015e5102b34a5a1016f
Martin Czygan <martin.czygan@gmail.com>
2019-03-20 19:37:31 +0100

    span-tag: allow to deduplicate against SOLR
    If -server is given (to a VuFind/SOLR), try to run clean ISIL
    attachments by comparing DOI with documents in index.
    
    A hacky way to drop labels by querying a SOLR server for DOI (we do not
    have a dedicated field yet, so query 'allfields', this can lead to
    problems with prefixes, too) and then compare source ids in index with
    preferences, adjustable via -prefs, similar to groupcover(1).
    
    This obviously takes longer, as we run one query for each DOI. The query
    is not yet optimized. To speed up processing, increase -w number of
    worker (and lower -b batch size, to not run out of memory).

--- cmd/span-tag/main.go
+++ cmd/span-tag/main.go
@@ -8,25 +8,23 @@
 // unpreferred source is indexed, we cannot currently update the index, so just
 // emit a warning and do not change anything.
 //
-// TODO(miku): Allow to skip label attachment by inspecting a SOLR index on the
-// fly. Calculate label attachments for record, query index for doi or similar
-// id, if the preferred source is already in the index, drop the label. If the
-// unpreferred source is indexed, we cannot currently update the index, so just
-// emit a warning and do not change anything.
-//
 // $ span-tag -c '{"DE-15": {"any": {}}}' < input.ldj > output.ldj
 //
 package main
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
+	"net/http"
+	"net/url"
 	"os"
 	"runtime"
 	"runtime/pprof"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 
@@ -36,26 +34,121 @@ import (
 	"github.com/miku/span/parallel"
 )
 
-// DroppableLabels returns a list of labels, that can be dropped with regard to
-// an index.
-func DroppableLabels(is finc.IntermediateSchema) (labels []string, err error) {
-	return
+var (
+	config     = flag.String("c", "", "JSON config file for filters")
+	version    = flag.Bool("v", false, "show version")
+	size       = flag.Int("b", 20000, "batch size")
+	numWorkers = flag.Int("w", runtime.NumCPU(), "number of workers")
+	cpuProfile = flag.String("cpuprofile", "", "write cpu profile to file")
+	unfreeze   = flag.String("unfreeze", "", "unfreeze filterconfig from a frozen file")
+	verbose    = flag.Bool("verbose", false, "verbose output")
+	server     = flag.String("server", "", "if not empty, query SOLR to deduplicate on-the-fly")
+	prefs      = flag.String("prefs", "85 55 89 60 50 105 34 101 53 49 28 48 121", "most preferred source id first, for deduplication")
+)
+
+// SelectResponse with reduced fields.
+type SelectResponse struct {
+	Response struct {
+		Docs []struct {
+			Institution []string `json:"institution"`
+			SourceID    string   `json:"source_id"`
+		} `json:"docs"`
+		NumFound int64 `json:"numFound"`
+		Start    int64 `json:"start"`
+	} `json:"response"`
+	ResponseHeader struct {
+		Params struct {
+			Q  string `json:"q"`
+			Wt string `json:"wt"`
+		} `json:"params"`
+		QTime  int64
+		Status int64 `json:"status"`
+	} `json:"responseHeader"`
+}
+
+// stringSliceContains returns true, if a given string is contained in a slice.
+func stringSliceContains(ss []string, s string) bool {
+	for _, v := range ss {
+		if v == s {
+			return true
+		}
+	}
+	return false
+}
+
+// preferencePosition returns the position of a given preference as int.
+// Smaller means preferred. If there is no match, return some higher number
+// (low prio).
+func preferencePosition(sid string) int {
+	fields := strings.Fields(*prefs)
+	for pos, v := range fields {
+		v = strings.TrimSpace(v)
+		if v == sid {
+			return pos
+		}
+	}
+	return 1000 // Or anything higher than the number of sources.
 }
 
 // DroppableLabels returns a list of labels, that can be dropped with regard to
-// an index.
+// an index. If document has no DOI, there is nothing to return.
 func DroppableLabels(is finc.IntermediateSchema) (labels []string, err error) {
+	doi := strings.TrimSpace(is.DOI)
+	if doi == "" {
+		return
+	}
+	link := fmt.Sprintf(`%s/select?df=allfields&wt=json&q="%s"`, *server, url.QueryEscape(doi))
+	if *verbose {
+		log.Println(link)
+	}
+	resp, err := http.Get(link)
+	if err != nil {
+		return labels, err
+	}
+	defer resp.Body.Close()
+	var sr SelectResponse
+
+	// Keep response for debugging.
+	var buf bytes.Buffer
+	tee := io.TeeReader(resp.Body, &buf)
+
+	if err := json.NewDecoder(tee).Decode(&sr); err != nil {
+		log.Printf("failed link: %s", link)
+		log.Printf("failed response: %s", buf.String())
+		return labels, err
+	}
+	for _, label := range is.Labels {
+		// For each label (ISIL), see, whether any match in SOLR has the same
+		// label (ISIL) as well.
+		for _, doc := range sr.Response.Docs {
+			// TODO(miku): we need to check all responses, find all source ids and then decide. Do we?
+			if !stringSliceContains(doc.Institution, label) {
+				continue
+			}
+			// The document (is) might be already in the index (same or other source).
+			if preferencePosition(is.SourceID) >= preferencePosition(doc.SourceID) {
+				// The prio position of the document is higher (means: lower prio). We may drop this label.
+				labels = append(labels, label)
+				break
+			} else {
+				log.Printf("%s (%s) has lower prio in index, but we cannot update index docs yet, skipping", is.ID, doi)
+			}
+		}
+	}
+	return labels, nil
+}
+
+// removeEach returns a new slice with element not in a drop list.
+func removeEach(ss []string, drop []string) (result []string) {
+	for _, s := range ss {
+		if !stringSliceContains(drop, s) {
+			result = append(result, s)
+		}
+	}
 	return
 }
 
 func main() {
-	config := flag.String("c", "", "JSON config file for filters")
-	version := flag.Bool("v", false, "show version")
-	size := flag.Int("b", 20000, "batch size")
-	numWorkers := flag.Int("w", runtime.NumCPU(), "number of workers")
-	cpuProfile := flag.String("cpuprofile", "", "write cpu profile to file")
-	unfreeze := flag.String("unfreeze", "", "unfreeze filterconfig from a frozen file")
-	server := flag.String("server", "", "if given, query SOLR to deduplicate on-the-fly")
 
 	flag.Parse()
 
@@ -130,14 +223,20 @@ func main() {
 
 		tagged := tagger.Tag(is)
 
-		// TODO(miku): If requested, inspect SOLR, we might be able to drop some labels.
+		// Deduplicate against a SOLR.
 		if *server != "" {
-			log.Fatal("not yet implemented")
-		}
-
-		// TODO(miku): If requested, inspect SOLR, we might be able to drop some labels.
-		if *server != "" {
-			log.Fatal("not yet implemented")
+			droppable, err := DroppableLabels(tagged)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if len(droppable) > 0 {
+				before := len(tagged.Labels)
+				tagged.Labels = removeEach(tagged.Labels, droppable)
+				if *verbose {
+					log.Printf("[%s] from %d to %d labels: %s",
+						is.ID, before, len(tagged.Labels), tagged.Labels)
+				}
+			}
 		}
 
 		bb, err := json.Marshal(tagged)
