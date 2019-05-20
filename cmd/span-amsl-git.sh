cf09d78bd6b1171d107495d62b64ea7d1ebf45b6
Martin Czygan <martin.czygan@gmail.com>
2019-05-20 18:26:49 +0200

    amsl-git: update docs

--- cmd/span-amsl-git.sh
+++ cmd/span-amsl-git.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
-
-# Fetch AMSL API JSON document and commit documents into a git repo. If run
-# regularly, this can serve as a log of changes.
 #
-#   $ span-amsl-git.sh https://example.amsl.technology /var/amsl-sync
+# Copyright 2019 by Leipzig University Library, http://ub.uni-leipzig.de
+#                   The Finc Authors, http://finc.info
+#                   Martin Czygan, <martin.czygan@uni-leipzig.de>
 #
-
+# This file is part of some open source application.
+#
+# Some open source application is free software: you can redistribute
+# it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# Some open source application is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+# @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+#
+#
+# Fetch AMSL API documents (API, holding files) and commit them into a git
+# repo. If run regularly, this can serve as a log of changes.
+#
+#   $ span-amsl-git.sh AMSL-API-URL WORK-TREE [GIT-DIR]
+#
+# Example:
+#
+#   $ span-amsl-git.sh https://example.amsl.technology /var/somerepo
+#
 set -e -o pipefail
 
 if [ "$#" -lt 2 ]; then
