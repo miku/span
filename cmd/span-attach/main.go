38ee8c1b1cc5512b4f52f9e2bd8b854ba31aed86
Martin Czygan <martin.czygan@gmail.com>
2020-02-19 22:14:16 +0100

    wip: tsv read

--- cmd/span-attach/main.go
+++ cmd/span-attach/main.go
@@ -1,4 +1,4 @@
-// span-attach -c config.tsv < in > out
+// $ span-attach -c config.tsv < in > out
 //
 // Does what span-tag did, but try to boil it down to a two step process: find
 // relevant lines in a tabular file for attachment (generated from AMSL or
@@ -11,12 +11,16 @@ package main
 
 import (
 	"flag"
+	"io"
 	"log"
+	"os"
+
+	"github.com/miku/span/encoding/tsv"
 )
 
 var (
-	force   = flag.Bool("f", false, "force all external referenced links to be downloaded")
-	tabConf = flag.String("c", "", "path to tabular config file (format, see: TBA)")
+	force      = flag.Bool("f", false, "force all external referenced links to be downloaded")
+	configFile = flag.String("c", "", "path to tabular config file (format, see: TBA)")
 )
 
 // An IntSet is a set of small, non-negative integers. Its zero value
@@ -40,22 +44,22 @@ func (t *ConfigTable) Append(row ConfigRow) {}
 
 // ConfigRow decribing a single entry (e.g. an attachment request).
 type ConfigRow struct {
-	ShardLabel                     string
-	ISIL                           string
-	SourceID                       string
-	TechnicalCollectionID          string
-	MegaCollection                 string
-	HoldingsFileURI                string
-	HoldingsFileLabel              string
-	LinkToHoldingsFile             string
-	EvaluateHoldingsFileForLibrary string
-	ContentFileURI                 string
-	ContentFileLabel               string
-	LinkToContentFile              string
-	ExternalLinkToContentFile      string
-	ProductISIL                    string
-	DokumentURI                    string
-	DokumentLabel                  string
+	ShardLabel                     string `tsv:"shard_label"`
+	ISIL                           string `tsv:"isil"`
+	SourceID                       string `tsv:"source_id"`
+	TechnicalCollectionID          string `tsv:"tcid"`
+	MegaCollection                 string `tsv:"mega_collection"`
+	HoldingsFileURI                string `tsv:"holdings_file_uri"`
+	HoldingsFileLabel              string `tsv:"holdings_file_label"`
+	LinkToHoldingsFile             string `tsv:"link_to_holdings_file"`
+	EvaluateHoldingsFileForLibrary string `tsv:"evaluate_holdings_file_for_library"`
+	ContentFileURI                 string `tsv:"content_file_uri"`
+	ContentFileLabel               string `tsv:"content_file_label"`
+	LinkToContentFile              string `tsv:"link_to_content_file"`
+	ExternalLinkToContentFile      string `tsv:"external_link_to_content_file"`
+	ProductISIL                    string `tsv:"product_isil"`
+	DokumentURI                    string `tsv:"dokument_uri"`
+	DokumentLabel                  string `tsv:"dokument_label"`
 }
 
 // Lookup wraps data for attachment lookups.
@@ -64,5 +68,45 @@ type Lookup struct {
 
 func main() {
 	flag.Parse()
-	log.Println("attach is the new tag")
+	if *configFile == "" {
+		log.Fatal("a config file is required")
+	}
+	f, err := os.Open(*configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	dec := tsv.NewDecoder(f)
+	// Setting header manually, since input file will not have headers.
+	dec.Header = []string{
+		"shard_label",
+		"isil",
+		"source_id",
+		"tcid",
+		"mega_collection",
+		"holdings_file_uri",
+		"holdings_file_label",
+		"link_to_holdings_file",
+		"evaluate_holdings_file_for_library",
+		"content_file_uri",
+		"content_file_label",
+		"link_to_content_file",
+		"external_link_to_content_file",
+		"product_isil",
+		"dokument_uri",
+		"dokument_label",
+	}
+	var table ConfigTable
+	for {
+		var row ConfigRow
+		err := dec.Decode(&row)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		table.Append(row)
+	}
+	log.Printf("%entries = %d", table.rows)
 }
