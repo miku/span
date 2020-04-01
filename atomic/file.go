f80b602c8354f813d1b23c4eb0e3ffa186fdfb72
Martin Czygan <martin.czygan@gmail.com>
2020-04-01 13:04:16 +0200

    add atomic file ops

---
+++ atomic/file.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+)
+
+// WriteFileAtomic writes the data to a temp file and atomically move if everything else succeeds.
+func WriteFile(filename string, data []byte, perm os.FileMode) error {
+	dir, name := path.Split(filename)
+	f, err := ioutil.TempFile(dir, name)
+	if err != nil {
+		return err
+	}
+	_, err = f.Write(data)
+	if err == nil {
+		err = f.Sync()
+	}
+	if closeErr := f.Close(); err == nil {
+		err = closeErr
+	}
+	if permErr := os.Chmod(f.Name(), perm); err == nil {
+		err = permErr
+	}
+	if err == nil {
+		err = os.Rename(f.Name(), filename)
+	}
+	// Any err should result in full cleanup.
+	if err != nil {
+		os.Remove(f.Name())
+	}
+	return err
+}
