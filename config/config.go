8e35be6139745e38ee5cf5a5720a8d5d43e68d03
Martin Czygan <martin.czygan@gmail.com>
2020-05-27 12:54:47 +0200

    add config stub

---
+++ config/config.go
@@ -0,0 +1,17 @@
+// Package config handles application configuration and location and loading of
+// various mapping files.
+//
+// Introduce new config location at /etc/span/assets/..., load data from file,
+// not from the binary itself (get rid of go-bindata).
+package config
+
+// Config is application configuration of span and its subcommands.
+type Config struct {
+	GitLabToken      string `yaml:"gitlab.token" env:"SPAN_GITLAB_TOKEN"`
+	RedmineToken     string `yaml:"redmine.token" env:"SPAN_REDMINE_TOKEN"`
+	RedmineURL       string `yaml:"redmine.url" env:"SPAN_REDMINE_URL"`
+	WebhookdHostPort string `yaml:"webhookd.listen" env:"SPAN_WEBHOOKD_LISTEN" env-default:"0.0.0.0:8080"`
+	WebhookdPath     string `yaml:"webhookd.path" env:"SPAN_WEBHOOKD_PATH" env-default:"trigger"`
+	WebhookdLogfile  string `yaml:"webhookd.logfile" env:"SPAN_WEBHOOKD_LOGFILE"`
+	WhatIsLiveURL    string `yaml:"whatislive.url" env:"SPAN_WHATISLIVE_URL"`
+}
