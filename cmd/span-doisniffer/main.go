4f2b4e7d92d2a2e9f372c0abe4d0a24afd748c93
Martin Czygan <martin.czygan@gmail.com>
2022-08-17 13:31:58 +0200

    add doisniffer command

---
+++ cmd/span-doisniffer/main.go
@@ -0,0 +1,102 @@
+// Sniff out DOI from JSON document, optionally update docs with found DOI.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"runtime"
+	"strings"
+
+	"github.com/miku/span/doi"
+)
+
+var (
+	Version   string
+	Buildtime string
+
+	noSkipUnmatched = flag.Bool("S", false, "do not skip unmatched documents")
+	updateKey       = flag.String("k", "doi_str_mv", "update key")
+	forceOverwrite  = flag.Bool("f", false, "force update, even if updateKey field exists")
+	identifierKey   = flag.String("i", "id", "identifier key")
+	ignoreKeys      = flag.String("K", "barcode,dewey", "ignore keys (regexp), comma separated") // TODO: repeated flag
+	numWorkers      = flag.Int("w", runtime.NumCPU(), "number of workers")
+	batchSize       = flag.Int("b", 5000, "batch size")
+	showVersion     = flag.Bool("version", false, "show version and exit")
+)
+
+func main() {
+	flag.Parse()
+	if *showVersion {
+		fmt.Printf("makta %s %s\n", Version, Buildtime)
+		os.Exit(0)
+	}
+	ignore, err := stringToRegexpSlice(*ignoreKeys, ",")
+	if err != nil {
+		log.Fatal(err)
+	}
+	sniffer := &doi.Sniffer{
+		Reader:        os.Stdin,
+		Writer:        os.Stdout,
+		SkipUnmatched: !*noSkipUnmatched,
+		UpdateKey:     *updateKey,
+		IdentifierKey: *identifierKey,
+		MapSniffer: &doi.MapSniffer{
+			Pattern:    regexp.MustCompile(doi.PatDOI),
+			IgnoreKeys: ignore,
+		},
+		// Custom postprocessing, cannot be changed from flags.
+		PostProcess: func(s string) string {
+			s = strings.TrimSpace(s)
+			switch {
+			case strings.HasSuffix(s, "])"):
+				// ai-179-z4p6s    10.24072/pci.ecology.100076])
+				return s[:len(s)-2]
+			case strings.HasSuffix(s, "/epdf"):
+				return s[:len(s)-5]
+			case strings.HasSuffix(s, ")") && !strings.Contains(s, "("):
+				// ai-179-wynjb    10.1016/j.jenvp.2019.01.011)
+				return s[:len(s)-1]
+			case strings.HasSuffix(s, "]") && !strings.Contains(s, "["):
+				// ai-28-29f64b012591451f83832a41c64bed83  10.5329/RECADM.20090802005]
+				return s[:len(s)-1]
+			case hasAnySuffix(s, []string{".", ",", ":", "*", `”`, "'"}):
+				return s[:len(s)-1]
+			default:
+				return s
+			}
+		},
+		NumWorkers: *numWorkers,
+		BatchSize:  *batchSize,
+	}
+	if err := sniffer.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// hasAnySuffix returns true, if s has any one of the given suffixes.
+func hasAnySuffix(s string, suffixes []string) bool {
+	for _, suffix := range suffixes {
+		if strings.HasSuffix(s, suffix) {
+			return true
+		}
+	}
+	return false
+}
+
+// stringToRegexpSlice converts a string into a list of compiled patterns.
+func stringToRegexpSlice(s string, sep string) (result []*regexp.Regexp, err error) {
+	if len(s) == 0 {
+		return
+	}
+	for _, v := range strings.Split(s, sep) {
+		re, err := regexp.Compile(v)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, re)
+	}
+	return result, nil
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/miku/span
 go 1.16
 
 require (
-	github.com/BurntSushi/toml v1.0.0 // indirect
+	github.com/BurntSushi/toml v1.2.0 // indirect
 	github.com/abadojack/whatlanggo v1.0.1
 	github.com/adrg/xdg v0.4.0
 	github.com/andrew-d/go-termutil v0.0.0-20150726205930-009166a695a2
@@ -14,31 +14,32 @@ require (
 	github.com/fatih/color v1.13.0
 	github.com/fatih/structs v1.1.0
 	github.com/goodsign/monday v1.0.0
+	github.com/google/go-cmp v0.5.8
 	github.com/gorilla/mux v1.8.0
 	github.com/hoisie/mustache v0.0.0-20160804235033-6375acf62c69 // indirect
-	github.com/ilyakaznacheev/cleanenv v1.2.6
-	github.com/jinzhu/now v1.1.4
-	github.com/jmoiron/sqlx v1.3.4
-	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/ilyakaznacheev/cleanenv v1.3.0
+	github.com/jinzhu/now v1.1.5
+	github.com/jmoiron/sqlx v1.3.5
 	github.com/kennygrant/sanitize v1.2.4
-	github.com/klauspost/compress v1.15.4
+	github.com/klauspost/compress v1.15.9
 	github.com/klauspost/pgzip v1.2.5
 	github.com/kr/pretty v0.3.0 // indirect
 	github.com/lytics/logrus v0.0.0-20170528191427-4389a17ed024
-	github.com/mattn/go-colorable v0.1.12 // indirect
-	github.com/mattn/go-sqlite3 v1.14.11
-	github.com/microcosm-cc/bluemonday v1.0.18
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-sqlite3 v1.14.15
+	github.com/microcosm-cc/bluemonday v1.0.19
 	github.com/miku/clam v0.0.0-20150701134247-8054bf3dfcec
 	github.com/miku/parallel v0.0.0-20210205192328-1a799ab70294
 	github.com/miku/xmlstream v0.0.0-20190415141048-c7ce7c45f0e0
 	github.com/mvdan/xurls v1.1.0 // indirect
-	github.com/olebedev/config v0.0.0-20190528211619-364964f3a8e4
-	github.com/segmentio/encoding v0.3.3
+	github.com/olebedev/config v0.0.0-20220816194630-7eaf6b4b0aa2
+	github.com/segmentio/asm v1.2.0 // indirect
+	github.com/segmentio/encoding v0.3.5
 	github.com/sethgrid/pester v1.2.0
 	github.com/shantanubhadoria/go-roman v0.0.0-20180925203848-b6cf86aa5b76
-	github.com/sirupsen/logrus v1.8.1
-	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd
-	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+	github.com/sirupsen/logrus v1.9.0
+	golang.org/x/net v0.0.0-20220812174116-3211cb980234
+	golang.org/x/sys v0.0.0-20220817070843-5a390386f1f2 // indirect
 	golang.org/x/text v0.3.7
 	gopkg.in/yaml.v2 v2.4.0
 	mvdan.cc/xurls v1.1.0
